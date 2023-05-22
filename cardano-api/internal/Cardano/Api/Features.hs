365134312ae5ec3835a0749fd279f19e7ab904c6
John Ky <john.ky@iohk.io>
2023-05-22 10:40:53 +1000

    Make supportedInEra whenMaybeSupportedInEra maybeSupportedInEra typesafe

--- cardano-api/internal/Cardano/Api/Features.hs
+++ cardano-api/internal/Cardano/Api/Features.hs
@@ -5,11 +5,13 @@
 module Cardano.Api.Features
   ( Feature(..)
   , supportedInEra
+  , untypedSupportedInEra
   , maybeSupportedInEra
   , whenMaybeSupportedInEra
   ) where
 
 import           Cardano.Api.Eras
+import           Cardano.Api.Value
 import           Data.Aeson (ToJSON (..))
 
 data ProtocolParameterUTxOCostPerWord
@@ -17,30 +19,30 @@ data ProtocolParameterUTxOCostPerWord
 data ProtocolParameterUTxOCostPerByte
 
 -- | A representation of a feature that is supported in a given era.
-data Feature feature where
-  ProtocolParameterUTxOCostPerWord :: Feature ProtocolParameterUTxOCostPerWord
-  ProtocolParameterUTxOCostPerByte :: Feature ProtocolParameterUTxOCostPerByte
+data Feature feature a where
+  ProtocolParameterUTxOCostPerWord :: Feature ProtocolParameterUTxOCostPerWord Lovelace
+  ProtocolParameterUTxOCostPerByte :: Feature ProtocolParameterUTxOCostPerByte Lovelace
 
-deriving instance Eq (Feature f)
-deriving instance Show (Feature f)
+deriving instance Eq (Feature f a)
+deriving instance Show (Feature f a)
 
-instance ToJSON (Feature f) where
+instance ToJSON (Feature f a) where
   toJSON = toJSON . show
 
 -- | Determine whether a feature is supported in a given era.
 --
 -- If the feature is not supported in the given era, 'no' is returned, otherwise 'yes'.
-supportedInEra
+untypedSupportedInEra
   :: a
   -- ^ The value to return if the feature is not supported in the given era.
   -> a
   -- ^ The value to return if the feature is supported in the given era.
-  -> Feature feature
+  -> Feature feature b
   -- ^ The feature to check.
   -> CardanoEra era
   -- ^ The era to check.
   -> a
-supportedInEra no yes = \case
+untypedSupportedInEra no yes = \case
   ProtocolParameterUTxOCostPerWord -> \case
     ByronEra    -> no
     ShelleyEra  -> no
@@ -60,17 +62,32 @@ supportedInEra no yes = \case
 
 -- | Determine whether a feature is supported in a given era.
 --
+-- If the feature is not supported in the given era, 'no' is returned, otherwise 'yes'.
+supportedInEra
+  :: a
+  -- ^ The value to return if the feature is not supported in the given era.
+  -> a
+  -- ^ The value to return if the feature is supported in the given era.
+  -> Feature feature a
+  -- ^ The feature to check.
+  -> CardanoEra era
+  -- ^ The era to check.
+  -> a
+supportedInEra = untypedSupportedInEra
+
+-- | Determine whether a feature is supported in a given era.
+--
 -- If the feature is not supported in the given era, 'pure Nothing' is returned, otherwise 'Just <$> fa'.
 whenMaybeSupportedInEra :: ()
   => Applicative f
   => f a
   -- ^ The value to return if the feature is not supported in the given era.
-  -> Feature feature
+  -> Feature feature a
   -- ^ The feature to check.
   -> CardanoEra era
   -- ^ The era to check.
   -> f (Maybe a)
-whenMaybeSupportedInEra fa = supportedInEra (pure Nothing) (Just <$> fa)
+whenMaybeSupportedInEra fa = untypedSupportedInEra (pure Nothing) (Just <$> fa)
 
 -- | Determine whether a feature is supported in a given era.
 --
@@ -78,9 +95,9 @@ whenMaybeSupportedInEra fa = supportedInEra (pure Nothing) (Just <$> fa)
 maybeSupportedInEra :: ()
   => a
   -- ^ The value to return if the feature is not supported in the given era.
-  -> Feature feature
+  -> Feature feature a
   -- ^ The feature to check.
   -> CardanoEra era
   -- ^ The era to check.
   -> Maybe a
-maybeSupportedInEra a = supportedInEra Nothing (Just a)
+maybeSupportedInEra a = untypedSupportedInEra Nothing (Just a)
