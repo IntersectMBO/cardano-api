10037474f249b345658e69ed0ae2d8d77a956d97
John Ky <john.ky@iohk.io>
2023-05-23 13:42:26 +1000

    New checkVrfFilePermissions function

--- cardano-api/internal/Cardano/Api/IO/Compat.hs
+++ cardano-api/internal/Cardano/Api/IO/Compat.hs
@@ -2,14 +2,17 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Cardano.Api.IO.Compat
-  ( handleFileForWritingWithOwnerPermission
+  ( checkVrfFilePermissions
+  , handleFileForWritingWithOwnerPermission
   , writeSecrets
   ) where
 
 import           Cardano.Api.Error
+import           Cardano.Api.IO.Base
 import           Cardano.Api.IO.Compat.Posix
 import           Cardano.Api.IO.Compat.Win32
 
+import           Control.Monad.Except (ExceptT)
 import           Data.ByteString (ByteString)
 import           System.IO
 
@@ -21,3 +24,6 @@ handleFileForWritingWithOwnerPermission = handleFileForWritingWithOwnerPermissio
 
 writeSecrets :: FilePath -> [Char] -> [Char] -> (a -> ByteString) -> [a] -> IO ()
 writeSecrets = writeSecretsImpl
+
+checkVrfFilePermissions :: File content direction -> ExceptT VRFPrivateKeyFilePermissionError IO ()
+checkVrfFilePermissions = checkVrfFilePermissionsImpl
