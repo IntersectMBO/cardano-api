84cbd87d9282b12d9d07f52a54debdd625c7a734
John Ky <john.ky@iohk.io>
2023-05-23 15:57:34 +1000

    Import cardano-cli project

---
+++ cardano-cli/test/cardano-cli-golden/Test/Golden/Byron/TxBody.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Golden.Byron.TxBody
+  ( golden_byronTxBody
+  ) where
+
+import           Hedgehog (Property)
+
+{- HLINT ignore "Use camelCase" -}
+
+golden_byronTxBody :: Property
+golden_byronTxBody = error "TODO"
