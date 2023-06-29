6007ef6bc891ae2c9d4f4d133f7b792b0a7d9355
John Ky <john.ky@iohk.io>
2023-06-30 08:48:52 +1000

    Parameterise Certificate type with phantom era type argument

--- cardano-api/internal/Cardano/Api/TxBody.hs
+++ cardano-api/internal/Cardano/Api/TxBody.hs
@@ -1693,7 +1693,7 @@ data TxCertificates build era where
      TxCertificatesNone :: TxCertificates build era
 
      TxCertificates     :: CertificatesSupportedInEra era
-                        -> [Certificate]
+                        -> [Certificate era]
                         -> BuildTxWith build
                              (Map StakeCredential (Witness WitCtxStake era))
                         -> TxCertificates build era
