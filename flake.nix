55374390050b5501e8ee4c1fd744fd215e540ef6
John Ky <john.ky@iohk.io>
2023-07-05 22:43:11 +1000

    Disable x86_64-darwin in nix due to crypto32 dependency issues blocking CI

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
   outputs = inputs: let
     supportedSystems = [
       "x86_64-linux"
-      "x86_64-darwin"
+      # "x86_64-darwin"
       # "aarch64-linux" - disable these temporarily because the build is broken
       # "aarch64-darwin" - disable these temporarily because the build is broken
     ];
