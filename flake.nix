10cde00a33589411186fce1b7da749c562ad5477
Hamish Mackenzie <Hamish.K.Mackenzie@gmail.com>
2023-07-07 17:49:50 +1200

    Work around cabal/haskel.nix issue with sublibs

--- flake.nix
+++ flake.nix
@@ -93,6 +93,7 @@
           shell.withHoogle = false;
           # Skip cross compilers for the shell
           shell.crossPlatforms = _: [];
+          shell.packages = p: [ p.cardano-api p.plutus-ledger-api p.ouroboros-consensus-cardano ];
 
           # package customizations as needed. Where cabal.project is not
           # specific enough, or doesn't allow setting these.
