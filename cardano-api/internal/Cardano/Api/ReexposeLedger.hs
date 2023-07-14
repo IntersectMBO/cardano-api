58d7f3ee4d92475b8f4447b7a6828addc7bbd33c
Jordan Millar <jordan.millar@iohk.io>
2023-07-14 15:13:32 -0400

    Introduce internal module Cardano.Api.ReexposeLedger. This module imports cardano-ledger's Cardano.Ledger.Api module and other necessary modules from cardano-ledger Introduce external module Cardano.Api.Ledger. This module reexports Cardano.Api.ReexposeLedger with the aim of transitioning cardano-api (and cardano-cli) to be fully reliant on cardano-ledger-api

---
+++ cardano-api/internal/Cardano/Api/ReexposeLedger.hs
@@ -0,0 +1,66 @@
+module Cardano.Api.ReexposeLedger
+  ( module Cardano.Ledger.Api
+  , Credential (..)
+  , KeyHash(..)
+  , KeyRole(..)
+  , ShelleyTxCert(..)
+  , ShelleyDelegCert(..)
+  , ShelleyEraTxCert(..)
+  , GenesisDelegCert(..)
+  , PoolParams (..)
+  , HasKeyRole(..)
+  , MIRPot(..)
+  , MIRTarget(..)
+  , MIRCert(..)
+  , StakePoolRelay(..)
+  , PoolMetadata(..)
+  , EraTxCert(..)
+
+  -- Core
+  , PoolCert(..)
+  , toEraCBOR
+  , fromEraCBOR
+
+  -- Conway
+  , ConwayTxCert(..)
+  , ConwayCommitteeCert(..)
+  , ConwayDelegCert(..)
+  , ConwayEraTxCert(..)
+
+  -- Base
+  , boundRational
+  , unboundRational
+  , DnsName
+  , dnsToText
+  , textToDns
+  , Url
+  , urlToText
+  , textToUrl
+  , portToWord16
+  , strictMaybeToMaybe
+  , maybeToStrictMaybe
+
+  -- Crypto
+  , hashToBytes
+  , hashFromBytes
+
+  -- Slotting
+  , EpochNo(..)
+  ) where
+
+import           Cardano.Crypto.Hash.Class (hashFromBytes, hashToBytes)
+import           Cardano.Ledger.Api
+import           Cardano.Ledger.BaseTypes (DnsName, Url, boundRational, dnsToText,
+                   maybeToStrictMaybe, portToWord16, strictMaybeToMaybe, textToDns, textToUrl,
+                   unboundRational, urlToText)
+import           Cardano.Ledger.Conway.TxCert (ConwayCommitteeCert (..), ConwayDelegCert (..),
+                   ConwayEraTxCert (..), ConwayTxCert (..))
+import           Cardano.Ledger.Core (PoolCert (..), fromEraCBOR, toEraCBOR)
+import           Cardano.Ledger.Credential (Credential (..))
+import           Cardano.Ledger.Keys (HasKeyRole (..), KeyHash (..), KeyRole (..))
+import           Cardano.Ledger.PoolParams (PoolMetadata (..), PoolParams (..), StakePoolRelay (..))
+import           Cardano.Ledger.Shelley.TxCert (EraTxCert (..), GenesisDelegCert (..), MIRCert (..),
+                   MIRPot (..), MIRTarget (..), ShelleyDelegCert (..), ShelleyEraTxCert (..),
+                   ShelleyTxCert (..))
+import           Cardano.Slotting.Slot (EpochNo (..))
+
---
+++ cardano-api/src/Cardano/Api/Ledger.hs
@@ -0,0 +1,6 @@
+module Cardano.Api.Ledger
+  ( module Cardano.Api.ReexposeLedger
+  )
+  where
+
+import           Cardano.Api.ReexposeLedger
