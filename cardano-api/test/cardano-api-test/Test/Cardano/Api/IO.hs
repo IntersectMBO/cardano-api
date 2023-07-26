71192c6194c915753969a5adbe8cfbf47cd2054f
John Ky <john.ky@iohk.io>
2023-07-26 23:29:48 +1000

    Test writeLazyByteStringFileWithOwnerPermissions

---
+++ cardano-api/test/cardano-api-test/Test/Cardano/Api/IO.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE TypeApplications #-}
+
+module Test.Cardano.Api.IO
+  ( tests
+  ) where
+
+import           Cardano.Api
+import           Cardano.Api.IO
+
+import           Control.Monad.Except (runExceptT)
+import           Control.Monad.IO.Class (liftIO)
+import           System.Directory (removeFile)
+
+import           Hedgehog
+import qualified Hedgehog.Extras as H
+import           Hedgehog.Internal.Property
+import           Test.Tasty (TestTree, testGroup)
+import           Test.Tasty.Hedgehog
+
+prop_createVrfFileWithOwnerPermissions :: Property
+prop_createVrfFileWithOwnerPermissions =
+  H.propertyOnce . H.moduleWorkspace "help" $ \ws -> do
+    file <- H.noteTempFile ws "file"
+
+    result <- liftIO $ writeLazyByteStringFileWithOwnerPermissions (File file) ""
+
+    case result of
+      Left err -> failWith Nothing $ displayError @(FileError ()) err
+      Right () -> return ()
+
+    fResult <- liftIO . runExceptT $ checkVrfFilePermissions (File file)
+
+    case fResult of
+      Left err -> failWith Nothing $ show err
+      Right () -> liftIO (removeFile file) >> success
+
+tests :: TestTree
+tests = testGroup "Test.Cardano.Api.IO"
+  [ testProperty "Create VRF File with Owner Permissions" prop_createVrfFileWithOwnerPermissions
+  ]
