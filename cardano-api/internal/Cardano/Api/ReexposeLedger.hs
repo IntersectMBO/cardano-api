a0457d32c7eedb1ae3e84fd636188da54982f420
Jordan Millar <jordan.millar@iohk.io>
2023-08-22 12:11:17 -0400

    Update Cardano.Api.ReexposeLedger to expose more ledger types

--- cardano-api/internal/Cardano/Api/ReexposeLedger.hs
+++ cardano-api/internal/Cardano/Api/ReexposeLedger.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module Cardano.Api.ReexposeLedger
-  ( module Cardano.Ledger.Api
-  , Credential (..)
+  ( Credential (..)
   , KeyHash(..)
   , KeyRole(..)
   , ShelleyTxCert(..)
@@ -16,23 +17,46 @@ module Cardano.Api.ReexposeLedger
   , PoolMetadata(..)
   , EraTxCert(..)
   , StrictMaybe(..)
+  , pattern DelegTxCert
+  , pattern RegPoolTxCert
+  , pattern RetirePoolTxCert
+  , pattern RegDepositTxCert
+  , pattern UnRegDepositTxCert
+  , pattern UnRegDRepTxCert
+  , pattern AuthCommitteeHotKeyTxCert
+  , pattern ResignCommitteeColdTxCert
+  , pattern RegTxCert
+  , pattern UnRegTxCert
+  , pattern RegDepositDelegTxCert
+  , pattern RegDRepTxCert
 
   -- Core
   , Coin (..)
   , EraCrypto
   , Network(..)
   , PoolCert(..)
+  , PParams(..)
   , addDeltaCoin
   , toDeltaCoin
   , toEraCBOR
   , fromEraCBOR
 
   -- Conway
+  , Delegatee(..)
   , DRep(..)
   , ConwayTxCert(..)
   , ConwayDelegCert(..)
   , ConwayEraTxCert(..)
   , ConwayGovCert(..)
+  , GovActionId(..)
+  , Vote (..)
+  , Voter (..)
+  , VotingProcedure(..)
+  -- Babbage
+  , CoinPerByte (..)
+
+  -- Alonzo
+  , CoinPerWord (..)
 
   -- Base
   , boundRational
@@ -50,25 +74,38 @@ module Cardano.Api.ReexposeLedger
   -- Crypto
   , hashToBytes
   , hashFromBytes
+  , Crypto
+  , StandardCrypto
 
   -- Slotting
   , EpochNo(..)
   ) where
 
 import           Cardano.Crypto.Hash.Class (hashFromBytes, hashToBytes)
-import           Cardano.Ledger.Api
+import           Cardano.Ledger.Alonzo.Core (CoinPerWord (..))
+import           Cardano.Ledger.Api.Tx.Cert (pattern AuthCommitteeHotKeyTxCert, pattern DelegTxCert,
+                   pattern RegDRepTxCert, pattern RegDepositDelegTxCert, pattern RegDepositTxCert,
+                   pattern RegPoolTxCert, pattern RegTxCert, pattern ResignCommitteeColdTxCert,
+                   pattern RetirePoolTxCert, pattern UnRegDRepTxCert, pattern UnRegDepositTxCert,
+                   pattern UnRegTxCert)
+import           Cardano.Ledger.Babbage.Core (CoinPerByte (..))
 import           Cardano.Ledger.BaseTypes (DnsName, Network (..), StrictMaybe (..), Url,
                    boundRational, dnsToText, maybeToStrictMaybe, portToWord16, strictMaybeToMaybe,
                    textToDns, textToUrl, unboundRational, urlToText)
 import           Cardano.Ledger.Coin (Coin (..), addDeltaCoin, toDeltaCoin)
+import           Cardano.Ledger.Conway.Governance (GovActionId (..), Vote (..), Voter (..),
+                   VotingProcedure (..))
 import           Cardano.Ledger.Conway.TxCert (ConwayDelegCert (..), ConwayEraTxCert (..),
-                   ConwayGovCert (..), ConwayTxCert (..))
-import           Cardano.Ledger.Core (DRep (..), EraCrypto, PoolCert (..), fromEraCBOR, toEraCBOR)
+                   ConwayGovCert (..), ConwayTxCert (..), Delegatee (..))
+import           Cardano.Ledger.Core (DRep (..), EraCrypto, PParams (..), PoolCert (..),
+                   fromEraCBOR, toEraCBOR)
 import           Cardano.Ledger.Credential (Credential (..))
+import           Cardano.Ledger.Crypto (Crypto, StandardCrypto)
 import           Cardano.Ledger.Keys (HasKeyRole (..), KeyHash (..), KeyRole (..))
 import           Cardano.Ledger.PoolParams (PoolMetadata (..), PoolParams (..), StakePoolRelay (..))
 import           Cardano.Ledger.Shelley.TxCert (EraTxCert (..), GenesisDelegCert (..), MIRCert (..),
                    MIRPot (..), MIRTarget (..), ShelleyDelegCert (..), ShelleyEraTxCert (..),
                    ShelleyTxCert (..))
 import           Cardano.Slotting.Slot (EpochNo (..))
+
 
