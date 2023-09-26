51f2a1e6e2cd7dcef2bc633d4dcbf0ef2901cfd9
John Ky <john.ky@iohk.io>
2023-09-26 22:46:58 +1000

    New ShelleyEraOnly eon

--- cardano-api/src/Cardano/Api.hs
+++ cardano-api/src/Cardano/Api.hs
@@ -72,6 +72,11 @@ module Cardano.Api (
 
     -- ** From Shelley
 
+    ShelleyEraOnly(..),
+    shelleyEraOnlyConstraints,
+    shelleyEraOnlyToCardanoEra,
+    shelleyEraOnlyToShelleyBasedEra,
+
     ShelleyToAllegraEra(..),
     shelleyToAllegraEraConstraints,
     shelleyToAllegraEraToCardanoEra,
@@ -1028,6 +1033,7 @@ import           Cardano.Api.Eon.ByronToMaryEra
 import           Cardano.Api.Eon.ConwayEraOnwards
 import           Cardano.Api.Eon.MaryEraOnwards
 import           Cardano.Api.Eon.ShelleyBasedEra
+import           Cardano.Api.Eon.ShelleyEraOnly
 import           Cardano.Api.Eon.ShelleyToAllegraEra
 import           Cardano.Api.Eon.ShelleyToAlonzoEra
 import           Cardano.Api.Eon.ShelleyToBabbageEra
