c5648f36a6e72526e2e3710a1a4807b028092fa7
John Ky <john.ky@iohk.io>
2023-09-29 20:35:22 +1000

    stuff

--- cardano-api/internal/Cardano/Api/Eon/Core2.hs
+++ cardano-api/internal/Cardano/Api/Eon/Core2.hs
@@ -6,11 +6,16 @@
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Cardano.Api.Eon.Core2 where
+
+import           Data.Proxy
+import           Data.Singletons
+import           GHC.TypeLits
 
 data E = Y | N deriving (Eq, Show)
 
@@ -29,6 +34,11 @@ type MaryOnwards        = '[N, N, N, Y, Y, Y, Y]
 type AlonzoOnwards      = '[N, N, N, N, Y, Y, Y]
 type BabbageOnwards     = '[N, N, N, N, N, Y, Y]
 type ConwayOnwards      = '[N, N, N, N, N, N, Y]
+
+type family Subset (as :: [E]) (bs :: [E]) :: Bool where
+  Subset '[] '[] = 'True
+  Subset (Y ': as) (N ': bs) = 'False
+  Subset (a ': as) (a ': bs) = Subset as bs
 
 type family Intersect (as :: [E]) (bs :: [E]) :: [E] where
   Intersect '[] '[] = '[]
@@ -55,3 +65,70 @@ type family Reverse (as :: [E]) :: [E] where
 
 type family Until (as :: [E]) :: [E] where
   Until as = Reverse (From (Reverse as))
+
+type family Minus (as :: [E]) (bs :: [E]) :: [E] where
+  Minus '[] '[] = '[]
+  Minus (a ': as) (Y ': bs) = N ': Minus as bs
+  Minus (a ': as) (N ': bs) = a ': Minus as bs
+
+data SubsetProof (as :: [E]) (bs :: [E]) where
+  SubsetProof :: IsSubset as bs => SubsetProof as bs
+
+class IsSubset (as :: [E]) (bs :: [E]) where
+  subsetProof :: SubsetProof as bs
+
+instance IsSubset '[] '[] where
+  subsetProof = SubsetProof
+
+instance IsSubset as bs => IsSubset (a ': as) (Y ': bs) where
+  subsetProof = SubsetProof
+
+instance {-# OVERLAPPING #-} IsSubset as bs => IsSubset (N ': as) (Y ': bs) where
+  subsetProof = SubsetProof
+
+data Era (eon :: [E]) where
+  Byron   :: Era Byron
+  Shelley :: Era Shelley
+  Allegra :: Era Allegra
+  Mary    :: Era Mary
+  Alonzo  :: Era Alonzo
+  Babbage :: Era Babbage
+  Conway  :: Era Conway
+
+data Eon (eras :: [E]) era where
+  Eon :: Era era -> Eon eras era
+
+relaxW :: SubsetProof as bs -> Eon as a -> Eon bs a
+relaxW _ (Eon a) = Eon a
+
+relax :: forall bs as a. IsSubset as bs => Eon as a -> Eon bs a
+relax (Eon a) = relaxW @as @bs subsetProof (Eon a)
+
+subset :: forall as bs. Proxy (as :: [E]) -> Proxy (bs :: [E]) -> Bool
+subset Proxy Proxy = demote @(Subset as bs :: Bool)
+
+class BoolValue (a :: Bool) where
+  boolValue :: Proxy a -> Bool
+
+instance BoolValue 'True where
+  boolValue _ = True
+
+instance BoolValue 'False where
+  boolValue _ = False
+
+case2 :: forall (bs :: [E]) (as :: [E]) era r eon. IsSubset bs as => (Eon bs era -> r) -> (Eon (Minus as bs) era -> r) -> Eon as era -> r
+case2 l r (Eon a) =
+  if demote @(Subset as bs :: Bool)
+    then l (Eon a)
+    else r (Eon a)
+
+example1 :: Eon ByronOnwards Byron -> Eon ByronOnwards Byron
+example1 = relax
+
+example2 :: Eon ShelleyOnwards Byron -> Eon ByronOnwards Byron
+example2 = relax
+
+-- -- Fails with: No instance for (Subset ByronOnwards ShelleyOnwards)
+-- example3 :: Eon ByronOnwards Byron -> Eon ShelleyOnwards Byron
+-- example3 = relax
+
