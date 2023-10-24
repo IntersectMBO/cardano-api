f1de93eb0f962e9ddebcdecb5df16cfb18dd1e70
teodanciu <teodora.danciu@tweag.io>
2023-10-24 12:36:02 +0100

    Export types needed for querying the committee state

--- cardano-api/src/Cardano/Api.hs
+++ cardano-api/src/Cardano/Api.hs
@@ -992,6 +992,9 @@ module Cardano.Api (
     queryDRepStakeDistribution,
     queryCommitteeMembersState,
 
+    -- ** Committee State Query
+    MemberStatus (..),
+    CommitteeMembersState (..),
     -- ** DReps
     DRepKey,
     DRepMetadata,
