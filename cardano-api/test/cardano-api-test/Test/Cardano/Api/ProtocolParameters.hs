1ee7a481f29e5287967ce0c0027220a5e5e84dce
Pablo Lamela <pablo.lamela@iohk.io>
2024-03-01 00:32:55 +0100

    Update to latest revision and patch JSON in tests

--- cardano-api/test/cardano-api-test/Test/Cardano/Api/ProtocolParameters.hs
+++ cardano-api/test/cardano-api-test/Test/Cardano/Api/ProtocolParameters.hs
@@ -2,17 +2,21 @@
 {-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 module Test.Cardano.Api.ProtocolParameters(tests) where
 
-import           Cardano.Api (CardanoEra (..), FromJSON, ProtocolParametersConversionError, ToJSON,
-                   inEonForEra, prettyPrintJSON)
+import           Cardano.Api (CardanoEra (..), ProtocolParametersConversionError, inEonForEra,
+                   prettyPrintJSON)
 import           Cardano.Api.Eon.ShelleyBasedEra (ShelleyBasedEra (..), ShelleyLedgerEra)
 import           Cardano.Api.Ledger (PParams (..))
 import           Cardano.Api.ProtocolParameters (LedgerProtocolParameters (..),
                    convertToLedgerProtocolParameters, fromLedgerPParams)
 
-import           Data.Aeson (eitherDecode)
+import           Data.Aeson (FromJSON, Object, ToJSON, decode, eitherDecode)
+import qualified Data.Aeson.Key as Aeson
+import qualified Data.Aeson.KeyMap as Aeson
 import qualified Data.ByteString.Lazy as LBS
+import           Data.Foldable (foldl')
 
 import           Test.Gen.Cardano.Api.Typed (genProtocolParameters)
 
@@ -47,7 +51,8 @@ protocolParametersSerializeTheSame era =
    property $ do ValidatedSerializedPair { serializedProtocolParams
                                          , serializedPParams
                                          } <- forAll $ genValidSerializedPair era
-                 serializedPParams === serializedProtocolParams
+                 patchedSerializedProtocolParams <- patchProtocolParamsJSONOrFail era serializedProtocolParams
+                 serializedPParams === patchedSerializedProtocolParams
 
 -- | Ensure that ProtocolParameter serialization can be deserialized by PParams FromJSON instance
 protocolParametersAreCompatible :: forall era. ( ToJSON (PParams (ShelleyLedgerEra era))
@@ -70,9 +75,10 @@ ppParamsRoundtrip era =
    property $ do ValidatedSerializedPair { serializedProtocolParams
                                          , serializedPParams = _
                                          } <- forAll $ genValidSerializedPair era
+                 patchedSerializedProtocolParams <- patchProtocolParamsJSONOrFail era serializedProtocolParams
                  case eitherDecode serializedProtocolParams :: Either String (PParams (ShelleyLedgerEra era)) of
                    Left err -> fail err
-                   Right pParams -> prettyPrintJSON pParams === LBS.toStrict serializedProtocolParams
+                   Right pParams -> prettyPrintJSON pParams === LBS.toStrict patchedSerializedProtocolParams
 
 -------------------------
 -- Auxiliary generator --
@@ -102,3 +108,38 @@ genValidSerializedPair era = do
 
       toShelleyBased :: CardanoEra era -> ShelleyBasedEra era
       toShelleyBased = inEonForEra (error "Not a Shelley-based era") id
+
+
+patchProtocolParamsJSONOrFail :: MonadFail m => CardanoEra era -> LBS.ByteString -> m LBS.ByteString
+patchProtocolParamsJSONOrFail era b = maybe (fail "Cannot fix JSON") return $ patchProtocolParamsJSON b
+  where
+    patchProtocolParamsJSON :: LBS.ByteString -> Maybe LBS.ByteString
+    patchProtocolParamsJSON s = LBS.fromStrict . prettyPrintJSON <$> (patchProtocolParamRepresentation =<< decode s)
+      where
+        patchProtocolParamRepresentation :: Object -> Maybe Object
+        patchProtocolParamRepresentation o = do filters <- filtersForEra era
+                                                replace "committeeTermLength" "committeeMaxTermLength"
+                                                  =<< replace "minCommitteeSize" "committeeMinSize"
+                                                        (applyFilters filters o)
+
+        filtersForEra :: CardanoEra era -> Maybe [String]
+        filtersForEra ShelleyEra = Just [ "collateralPercentage", "costModels", "executionUnitPrices"
+                                        , "maxBlockExecutionUnits", "maxCollateralInputs", "maxTxExecutionUnits"
+                                        , "maxValueSize", "utxoCostPerByte" ]
+        filtersForEra AlonzoEra  = Just [ "minUTxOValue" ]
+        filtersForEra BabbageEra = Just [ "decentralization", "extraPraosEntropy", "minUTxOValue" ]
+        filtersForEra _ = Nothing
+
+        applyFilters :: [String] -> Object -> Object
+        applyFilters filters o = foldl' (flip Aeson.delete) o (map Aeson.fromString filters)
+
+        replace :: String -> String -> Object -> Maybe Object
+        replace src dest o =
+          let srcKey = Aeson.fromString src
+              destKey = Aeson.fromString dest in
+          case Aeson.lookup srcKey o of
+            Nothing -> Just o
+            Just v -> if Aeson.member destKey o
+                      then Nothing
+                      else Just $ Aeson.insert destKey v $ Aeson.delete srcKey o
+
