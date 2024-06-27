3e2dc23a2ee2a3ef1c24a1641fac7db3a143b402
Pablo Lamela <pablo.lamela@iohk.io>
2024-06-27 22:05:43 +0200

    Make empty PoolDistr constructively

--- cardano-api/test/cardano-api-golden/Test/Golden/Cardano/Api/EpochLeadership.hs
+++ cardano-api/test/cardano-api-golden/Test/Golden/Cardano/Api/EpochLeadership.hs
@@ -9,18 +9,22 @@ import           Cardano.Api.Block (EpochNo (..), Hash (StakePoolKeyHash), SlotN
 import           Cardano.Api.Eon.ShelleyBasedEra (ShelleyBasedEra (..))
 import           Cardano.Api.Genesis (shelleyGenesisDefaults)
 import           Cardano.Api.GenesisParameters (EpochSize (..))
-import           Cardano.Api.Ledger (KeyHash (KeyHash))
+import           Cardano.Api.Ledger (KeyHash (..), StandardCrypto)
 import           Cardano.Api.LedgerState (currentEpochEligibleLeadershipSlots)
 import           Cardano.Api.Query (ProtocolState (..),
                    SerialisedPoolDistribution (SerialisedPoolDistribution))
 import           Cardano.Api.Shelley (VrfKey, proxyToAsType, unStakePoolKeyHash)
 
+import           Cardano.Binary (serialize)
 import           Cardano.Crypto.Seed (mkSeedFromBytes)
 import           Cardano.Ledger.Api.PParams (emptyPParams)
+import           Cardano.Ledger.Binary.Encoding (toByronCBOR)
 import           Cardano.Slotting.EpochInfo (EpochInfo (..))
 import           Cardano.Slotting.Time (RelativeTime (..), mkSlotLength)
+import           Ouroboros.Consensus.Shelley.Ledger.Query.Types (PoolDistr (..))
 import           Ouroboros.Network.Block (Serialised (..))
 
+import qualified Data.Map as Map
 import           Data.Proxy (Proxy (..))
 import qualified Data.Set as Set
 import           Data.Time.Clock (secondsToNominalDiffTime)
@@ -44,7 +48,8 @@ test_golden_currentEpochEligibleLeadershipSlots = testProperty "golden EpochLead
         ptclState = ProtocolState (Serialised "dummyProtocolState")
         poolid = StakePoolKeyHash { unStakePoolKeyHash = KeyHash "58eef2925db2789f76ea057c51069e52c5e0a44550f853c6cdf620f8" }
         vrskey = deterministicSigningKey (proxyToAsType (Proxy :: Proxy VrfKey)) (mkSeedFromBytes "")
-        serPoolDistr = SerialisedPoolDistribution (Serialised "dummyPoolDistr")
+        poolDistr :: PoolDistr StandardCrypto = PoolDistr Map.empty
+        serPoolDistr = SerialisedPoolDistribution (Serialised (serialize (toByronCBOR poolDistr)))
         currentEpoch = EpochNo 4
         eEligibileSlots = currentEpochEligibleLeadershipSlots sbe sGen eInfo pp ptclState poolid vrskey serPoolDistr currentEpoch
         expectedEligibleSlots = [SlotNo 2, SlotNo 6]
