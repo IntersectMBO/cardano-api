60e819cd33b56aebb76c6dc22e1c00e8c3560f99
Jordan Millar <jordan.millar@iohk.io>
2024-07-31 14:00:11 +0200

    Deprecate eras prior to BabbageEra in constructBalancedTx

--- cardano-api/internal/Cardano/Api/Tx/Body.hs
+++ cardano-api/internal/Cardano/Api/Tx/Body.hs
@@ -132,6 +132,28 @@ module Cardano.Api.Tx.Body
   , scriptDataToInlineDatum
 
     -- * Internal conversion functions & types
+  , convCertificates
+  , convCollateralTxIns
+  , convExtraKeyWitnesses
+  , convLanguages
+  , convMintValue
+  , convProposalProcedures
+  , convReferenceInputs
+  , convReturnCollateral
+  , convScripts
+  , convScriptData
+  , convTotalCollateral
+  , convTransactionFee
+  , convTxIns
+  , convTxOuts
+  , convTxUpdateProposal
+  , convValidityLowerBound
+  , convValidityUpperBound
+  , convVotingProcedures
+  , convWithdrawals
+  , getScriptIntegrityHash
+  , mkCommonTxBody
+  , toAuxiliaryData
   , toByronTxId
   , toShelleyTxId
   , toShelleyTxIn
