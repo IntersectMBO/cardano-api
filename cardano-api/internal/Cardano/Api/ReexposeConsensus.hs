7f33f4ca16697d592d4b2018fe6fc2e9d49f27c2
Pablo Lamela <pablo.lamela@iohk.io>
2024-10-30 14:22:35 +0100

    Reexport requirements from `ouroboros-consensus-protocol` by `cardano-cli`

---
+++ cardano-api/internal/Cardano/Api/ReexposeConsensus.hs
@@ -0,0 +1,4 @@
+module Cardano.Api.ReexposeConsensus (PraosProtocolSupportsNode, PraosProtocolSupportsNodeCrypto, getOpCertCounters) where
+
+import           Ouroboros.Consensus.Protocol.Praos.Common (PraosProtocolSupportsNode,
+                   PraosProtocolSupportsNodeCrypto, getOpCertCounters)
