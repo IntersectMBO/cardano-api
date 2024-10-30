25eb28ead6d84a01abc46fdb16d240a27175098c
Pablo Lamela <pablo.lamela@iohk.io>
2024-10-30 13:16:04 +0100

    Reexport requirements from `ouroboros-network-protocols` by `cardano-cli`

--- cardano-api/internal/Cardano/Api/ReexposeNetwork.hs
+++ cardano-api/internal/Cardano/Api/ReexposeNetwork.hs
@@ -1,3 +1,4 @@
-module Cardano.Api.ReexposeNetwork (Target (..)) where
+module Cardano.Api.ReexposeNetwork (Target (..), SubmitResult (..)) where
 
 import           Ouroboros.Network.Protocol.LocalStateQuery.Type (Target (..))
+import           Ouroboros.Network.Protocol.LocalTxSubmission.Type (SubmitResult (..))
