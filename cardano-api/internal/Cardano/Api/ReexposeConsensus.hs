5882afe2f8a4d1aefe917b9f849d0a746494113f
Pablo Lamela <pablo.lamela@iohk.io>
2024-10-30 16:16:15 +0100

    Reexport requirements from `ouroboros-consensus` by `cardano-cli`

--- cardano-api/internal/Cardano/Api/ReexposeConsensus.hs
+++ cardano-api/internal/Cardano/Api/ReexposeConsensus.hs
@@ -1,17 +1,29 @@
 module Cardano.Api.ReexposeConsensus
   ( ByronBlock
+  , ChainDepState
   , GenTx (..)
   , EraMismatch (..)
+  , PastHorizonException
   , PraosProtocolSupportsNode
   , PraosProtocolSupportsNodeCrypto
   , ShelleyGenesisStaking (..)
   , byronIdTx
+  , condense
   , getOpCertCounters
+  , interpreterToEpochInfo
+  , unsafeExtendSafeZone
+  , txId
   )
 where
 
 import           Ouroboros.Consensus.Byron.Ledger (ByronBlock, GenTx (..), byronIdTx)
 import           Ouroboros.Consensus.Cardano.Block (EraMismatch (..))
+import           Ouroboros.Consensus.HardFork.History.EpochInfo (interpreterToEpochInfo)
+import           Ouroboros.Consensus.HardFork.History.Qry (PastHorizonException,
+                   unsafeExtendSafeZone)
+import           Ouroboros.Consensus.Ledger.SupportsMempool (txId)
+import           Ouroboros.Consensus.Protocol.Abstract (ChainDepState)
 import           Ouroboros.Consensus.Protocol.Praos.Common (PraosProtocolSupportsNode,
                    PraosProtocolSupportsNodeCrypto, getOpCertCounters)
 import           Ouroboros.Consensus.Shelley.Node (ShelleyGenesisStaking (..))
+import           Ouroboros.Consensus.Util.Condense (condense)
