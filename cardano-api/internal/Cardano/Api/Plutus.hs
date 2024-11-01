5017812c85d8dca8e6cf1fa3a63481ff1b2e49c6
Jordan Millar <jordan.millar@iohk.io>
2024-11-01 14:02:05 -0400

    Introduce DebugPlutusFailure and renderDebugPlutusFailure

---
+++ cardano-api/internal/Cardano/Api/Plutus.hs
@@ -0,0 +1,54 @@
+module Cardano.Api.Plutus
+  ( DebugPlutusFailure (..)
+  , renderDebugPlutusFailure
+  )
+where
+
+import           Cardano.Api.Pretty
+
+import qualified Cardano.Ledger.Api as L
+import           Cardano.Ledger.Binary.Plain (serializeAsHexText)
+import qualified Cardano.Ledger.Plutus.Evaluate as Plutus
+import qualified Cardano.Ledger.Plutus.ExUnits as Plutus
+import qualified Cardano.Ledger.Plutus.Language as Plutus
+import qualified PlutusLedgerApi.V1 as Plutus
+
+import qualified Data.ByteString.Base64 as B64
+import           Data.ByteString.Short as BSS
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+
+data DebugPlutusFailure
+  = DebugPlutusFailure
+  { dpfEvaluationError :: Plutus.EvaluationError
+  , dpfScriptWithContext :: Plutus.PlutusWithContext L.StandardCrypto
+  , dpfExecutionUnits :: Plutus.ExUnits
+  , dpfExecutionLogs :: [Text]
+  }
+  deriving (Eq, Show)
+
+renderDebugPlutusFailure :: DebugPlutusFailure -> Text
+renderDebugPlutusFailure dpf =
+  let pwc = dpfScriptWithContext dpf
+      lang = case pwc of
+        Plutus.PlutusWithContext{Plutus.pwcScript = script} ->
+          either Plutus.plutusLanguage Plutus.plutusLanguage script
+
+      scriptArgs = case pwc of
+        Plutus.PlutusWithContext{Plutus.pwcArgs = args} ->
+          pretty args
+
+      evalError = dpfEvaluationError dpf
+      binaryScript = case pwc of
+        Plutus.PlutusWithContext{Plutus.pwcScript = scr} ->
+          let Plutus.Plutus bytes = either id Plutus.plutusFromRunnable scr
+           in Text.decodeUtf8 . B64.encode . BSS.fromShort $ Plutus.unPlutusBinary bytes
+   in Text.unlines
+        [ "Script hash: " <> serializeAsHexText (Plutus.pwcScriptHash pwc)
+        , "Script language: " <> Text.pack (show lang)
+        , "Protocol version: " <> Text.pack (show (Plutus.pwcProtocolVersion pwc))
+        , "Script arguments: " <> docToText scriptArgs
+        , "Script evaluation error: " <> docToText (pretty evalError)
+        , "Script base64 encoded bytes: " <> binaryScript
+        ]
