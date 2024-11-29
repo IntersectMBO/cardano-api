2f1b48bcb1456b3204274ad241d5b0877a5ca42e
Pablo Lamela <pablo.lamela@iohk.io>
2024-11-29 18:15:19 +0100

    More partial work (created Tx using experimental API)

--- cardano-api/test/cardano-api-test/Test/Cardano/Api/Experimental.hs
+++ cardano-api/test/cardano-api-test/Test/Cardano/Api/Experimental.hs
@@ -1,51 +1,92 @@
-{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Test.Cardano.Api.Experimental
   ( tests
   )
 where
 
-import Test.Tasty (TestTree, testGroup)
-import Hedgehog (Property, success)
-import Test.Tasty.Hedgehog (testProperty)
-import Hedgehog.Extras (propertyOnce)
 import qualified Cardano.Api as Api
 import qualified Cardano.Api.Experimental as Exp
-import Control.Monad.IO.Class (liftIO)
-import Lens.Micro ((&))
-import Cardano.Api.Tx.Body ( setTxIns, setTxFee, setTxOuts )
-import Cardano.Api (TxFee(TxFeeExplicit))
-import Cardano.Api.Ledger (Coin(Coin))
+import qualified Cardano.Api.Ledger as Ledger
+import qualified Cardano.Api.Script as Script
+
+import           Lens.Micro ((&))
+
+import           Hedgehog (Property)
+import qualified Hedgehog as H
+import qualified Hedgehog.Extras as H
+import           Test.Tasty (TestTree, testGroup)
+import           Test.Tasty.Hedgehog (testProperty)
 
 -- | Tests in this module can be run by themselves by writing:
 -- ```bash
 -- cabal test cardano-api-test --test-options="--pattern=Test.Cardano.Api.Experimental"
 -- ```
-
 tests :: TestTree
 tests =
   testGroup
     "Test.Cardano.Api.Experimental"
-    [ testProperty "Create transaction with experimental API" prop_create_transaction_with_experimental_api
+    [ testProperty
+        "Create transaction with experimental API"
+        prop_create_transaction_with_experimental_api
     ]
 
 prop_create_transaction_with_experimental_api :: Property
-prop_create_transaction_with_experimental_api = propertyOnce $ do
+prop_create_transaction_with_experimental_api = H.propertyOnce $ do
   let era = Exp.ConwayEra
   let sbe = Api.convert era
-  let txBodyContent = Api.defaultTxBodyContent sbe
-                        & setTxIns []
-                        & setTxOuts []
-                        & setTxFee (TxFeeExplicit sbe (Coin 2_000_000))
 
-  let unsignedTx = Exp.makeUnsignedTx era txBodyContent
+  srcTxId <-
+    H.evalEither $
+      Api.deserialiseFromRawBytesHex
+        Api.AsTxId
+        "be6efd42a3d7b9a00d09d77a5d41e55ceaf0bd093a8aa8a893ce70d9caafd978"
+  let srcTxIx = Api.TxIx 0
+  destAddress <-
+    H.evalMaybe $
+      Api.deserialiseAddress
+        (Api.AsAddressInEra Api.AsConwayEra)
+        "addr_test1vzpfxhjyjdlgk5c0xt8xw26avqxs52rtf69993j4tajehpcue4v2v"
+  signingKey <-
+    H.evalEither $
+      Api.deserialiseFromBech32
+        (Api.AsSigningKey Api.AsPaymentKey)
+        "addr_sk1648253w4tf6fv5fk28dc7crsjsaw7d9ymhztd4favg3cwkhz7x8sl5u3ms"
 
-  case unsignedTx of
-    Left e -> liftIO $ putStrLn $ "Error: " <> show e
-    Right tx -> do
-      let bootstrapWitnesses = []
-          keyWitnesses = []
-      let signedTx = Exp.signTx era bootstrapWitnesses keyWitnesses tx
-      liftIO $ print signedTx
+  let txBodyContent =
+        Api.defaultTxBodyContent sbe
+          & Api.setTxIns
+            [
+              ( Api.TxIn srcTxId srcTxIx
+              , Api.BuildTxWith (Api.KeyWitness Api.KeyWitnessForSpending)
+              )
+            ]
+          & Api.setTxOuts
+            [ Api.TxOut
+                destAddress
+                (Api.TxOutValueShelleyBased sbe (Api.inject (Ledger.Coin 10_000_000)))
+                Api.TxOutDatumNone
+                Script.ReferenceScriptNone
+            ]
+          & Api.setTxFee (Api.TxFeeExplicit sbe (Ledger.Coin 2_000_000))
+
+  unsignedTx <- H.evalEither $ Exp.makeUnsignedTx era txBodyContent
+  let witness = Exp.makeKeyWitness era unsignedTx (Api.WitnessPaymentKey signingKey)
+
+  let bootstrapWitnesses = []
+      keyWitnesses = [witness]
+
+  let signedTx :: Ledger.Tx (Exp.LedgerEra Api.ConwayEra) = Exp.signTx era bootstrapWitnesses keyWitnesses unsignedTx
   
-  success
+  -- The following line gives the following compilation type error:
+  --  Script.ReferenceScriptNone
+  -- • No instance for ‘Api.HasTextEnvelope
+  --                      (Cardano.Ledger.Alonzo.Tx.AlonzoTx
+  --                         (cardano-ledger-conway-1.17.0.0:Cardano.Ledger.Conway.Era.ConwayEra
+  --                            Ledger.StandardCrypto))’
+  --     arising from a use of ‘Api.textEnvelopeToJSON’
+
+  H.note_ $ show $ Api.textEnvelopeToJSON Nothing signedTx
+
+  fail "TODO: implement this test"
