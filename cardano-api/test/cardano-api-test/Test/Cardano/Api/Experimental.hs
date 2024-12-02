fac4b1c9e506daf2eb5983bdbdc4a85c656301f8
Pablo Lamela <pablo.lamela@iohk.io>
2024-12-02 16:21:13 +0100

    Refactor code and compare both results

--- cardano-api/test/cardano-api-test/Test/Cardano/Api/Experimental.hs
+++ cardano-api/test/cardano-api-test/Test/Cardano/Api/Experimental.hs
@@ -1,6 +1,6 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE FlexibleContexts #-}
 
 module Test.Cardano.Api.Experimental
   ( tests
@@ -8,19 +8,19 @@ module Test.Cardano.Api.Experimental
 where
 
 import qualified Cardano.Api as Api
+import           Cardano.Api.Eon.ShelleyBasedEra (ShelleyBasedEraConstraints)
 import qualified Cardano.Api.Experimental as Exp
 import qualified Cardano.Api.Ledger as Ledger
 import qualified Cardano.Api.Script as Script
+import           Cardano.Api.Tx.Sign (Tx (ShelleyTx))
 
 import           Lens.Micro ((&))
 
-import           Hedgehog (Property, success)
+import           Hedgehog (Property)
 import qualified Hedgehog as H
 import qualified Hedgehog.Extras as H
 import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.Hedgehog (testProperty)
-
-import Cardano.Api.Eon.ShelleyBasedEra (ShelleyBasedEraConstraints)
 
 -- | Tests in this module can be run by themselves by writing:
 -- ```bash
@@ -31,87 +31,92 @@ tests =
   testGroup
     "Test.Cardano.Api.Experimental"
     [ testProperty
-        "Create transaction with traditional API"
-        prop_create_transaction_with_traditional_api
-    , testProperty
-        "Create transaction with experimental API"
-        prop_create_transaction_with_experimental_api
+        "Created transaction with traditional and experimental APIs are equivalent"
+        prop_created_transaction_with_both_apis_are_the_same
     ]
 
-exampleTxBodyContent :: (ShelleyBasedEraConstraints era, H.MonadTest m)
-                     => Api.AsType era
-                     -> Api.ShelleyBasedEra era
-                     -> m (Api.TxBodyContent Api.BuildTx era)
-exampleTxBodyContent eraAsType sbe = do
-  srcTxId <-
+prop_created_transaction_with_both_apis_are_the_same :: Property
+prop_created_transaction_with_both_apis_are_the_same = H.propertyOnce $ do
+  let era = Exp.ConwayEra
+  let sbe = Api.convert era
+
+  signedTxTraditional <- exampleTransacitonTraditionalWay sbe
+  signedTxExperimental <- exampleTransactionExperimentalWay era sbe
+
+  let oldStyleTx :: Api.Tx Api.ConwayEra = ShelleyTx sbe signedTxExperimental
+
+  oldStyleTx H.=== signedTxTraditional
+ where
+  exampleTxBodyContent
+    :: (ShelleyBasedEraConstraints era, H.MonadTest m)
+    => Api.AsType era
+    -> Api.ShelleyBasedEra era
+    -> m (Api.TxBodyContent Api.BuildTx era)
+  exampleTxBodyContent eraAsType sbe = do
+    srcTxId <-
+      H.evalEither $
+        Api.deserialiseFromRawBytesHex
+          Api.AsTxId
+          "be6efd42a3d7b9a00d09d77a5d41e55ceaf0bd093a8aa8a893ce70d9caafd978"
+    let srcTxIx = Api.TxIx 0
+    destAddress <-
+      H.evalMaybe $
+        Api.deserialiseAddress
+          (Api.AsAddressInEra eraAsType)
+          "addr_test1vzpfxhjyjdlgk5c0xt8xw26avqxs52rtf69993j4tajehpcue4v2v"
+
+    let txBodyContent =
+          Api.defaultTxBodyContent sbe
+            & Api.setTxIns
+              [
+                ( Api.TxIn srcTxId srcTxIx
+                , Api.BuildTxWith (Api.KeyWitness Api.KeyWitnessForSpending)
+                )
+              ]
+            & Api.setTxOuts
+              [ Api.TxOut
+                  destAddress
+                  (Api.TxOutValueShelleyBased sbe (Api.inject (Ledger.Coin 10_000_000)))
+                  Api.TxOutDatumNone
+                  Script.ReferenceScriptNone
+              ]
+            & Api.setTxFee (Api.TxFeeExplicit sbe (Ledger.Coin 2_000_000))
+
+    return txBodyContent
+
+  exampleSigningKey :: H.MonadTest m => m (Api.SigningKey Api.PaymentKey)
+  exampleSigningKey =
     H.evalEither $
-      Api.deserialiseFromRawBytesHex
-        Api.AsTxId
-        "be6efd42a3d7b9a00d09d77a5d41e55ceaf0bd093a8aa8a893ce70d9caafd978"
-  let srcTxIx = Api.TxIx 0
-  destAddress <-
-    H.evalMaybe $
-      Api.deserialiseAddress
-        (Api.AsAddressInEra eraAsType)
-        "addr_test1vzpfxhjyjdlgk5c0xt8xw26avqxs52rtf69993j4tajehpcue4v2v"
-
-  let txBodyContent =
-        Api.defaultTxBodyContent sbe
-          & Api.setTxIns
-            [
-              ( Api.TxIn srcTxId srcTxIx
-              , Api.BuildTxWith (Api.KeyWitness Api.KeyWitnessForSpending)
-              )
-            ]
-          & Api.setTxOuts
-            [ Api.TxOut
-                destAddress
-                (Api.TxOutValueShelleyBased sbe (Api.inject (Ledger.Coin 10_000_000)))
-                Api.TxOutDatumNone
-                Script.ReferenceScriptNone
-            ]
-          & Api.setTxFee (Api.TxFeeExplicit sbe (Ledger.Coin 2_000_000))
-
-  return txBodyContent
-
-exampleSigningKey :: H.MonadTest m => m (Api.SigningKey Api.PaymentKey)
-exampleSigningKey =
-  H.evalEither $
       Api.deserialiseFromBech32
         (Api.AsSigningKey Api.AsPaymentKey)
         "addr_sk1648253w4tf6fv5fk28dc7crsjsaw7d9ymhztd4favg3cwkhz7x8sl5u3ms"
 
-prop_create_transaction_with_traditional_api :: Property
-prop_create_transaction_with_traditional_api = H.propertyOnce $ do
-  let sbe = Api.ShelleyBasedEraConway
+  exampleTransacitonTraditionalWay
+    :: H.MonadTest m => Api.ShelleyBasedEra Exp.ConwayEra -> m (Tx Exp.ConwayEra)
+  exampleTransacitonTraditionalWay sbe = do
+    txBodyContent <- exampleTxBodyContent Api.AsConwayEra sbe
+    signingKey <- exampleSigningKey
 
-  txBodyContent <- exampleTxBodyContent Api.AsConwayEra sbe
-  signingKey <- exampleSigningKey
+    txBody <- H.evalEither $ Api.createTransactionBody sbe txBodyContent
 
-  txBody <- H.evalEither $ Api.createTransactionBody sbe txBodyContent
+    let signedTx :: Api.Tx Api.ConwayEra = Api.signShelleyTransaction sbe txBody [Api.WitnessPaymentKey signingKey]
 
-  let signedTx :: Api.Tx Api.ConwayEra = Api.signShelleyTransaction sbe txBody [Api.WitnessPaymentKey signingKey]
+    return signedTx
 
-  H.note_ $ show $ Api.textEnvelopeToJSON Nothing signedTx
+  exampleTransactionExperimentalWay
+    :: H.MonadTest m
+    => Exp.Era Exp.ConwayEra
+    -> Api.ShelleyBasedEra Exp.ConwayEra
+    -> m (Ledger.Tx (Exp.LedgerEra Exp.ConwayEra))
+  exampleTransactionExperimentalWay era sbe = do
+    txBodyContent <- exampleTxBodyContent Api.AsConwayEra sbe
+    signingKey <- exampleSigningKey
 
-  success
+    unsignedTx <- H.evalEither $ Exp.makeUnsignedTx era txBodyContent
+    let witness = Exp.makeKeyWitness era unsignedTx (Api.WitnessPaymentKey signingKey)
 
-prop_create_transaction_with_experimental_api :: Property
-prop_create_transaction_with_experimental_api = H.propertyOnce $ do
-  let era = Exp.ConwayEra
-  let sbe = Api.convert era
+    let bootstrapWitnesses = []
+        keyWitnesses = [witness]
 
-  txBodyContent <- exampleTxBodyContent Api.AsConwayEra sbe
-  signingKey <- exampleSigningKey
-
-  unsignedTx <- H.evalEither $ Exp.makeUnsignedTx era txBodyContent
-  let witness = Exp.makeKeyWitness era unsignedTx (Api.WitnessPaymentKey signingKey)
-
-  let bootstrapWitnesses = []
-      keyWitnesses = [witness]
-
-  let _signedTx :: Ledger.Tx (Exp.LedgerEra Exp.ConwayEra) = Exp.signTx era bootstrapWitnesses keyWitnesses unsignedTx
-
-  -- H.note_ $ show $ Api.textEnvelopeToJSON Nothing signedTx
-
-  success
+    let signedTx :: Ledger.Tx (Exp.LedgerEra Exp.ConwayEra) = Exp.signTx era bootstrapWitnesses keyWitnesses unsignedTx
+    return signedTx
