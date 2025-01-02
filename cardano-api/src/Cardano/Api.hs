7f5862cf5771dfb55228209d72b53d7ef4df7280
Daniel Firth <daniel.firth@iohk.io>
2025-01-02 15:49:50 +0000

    Re-export DebugPlutusFailure and renderDebugPlutusFailure

--- cardano-api/src/Cardano/Api.hs
+++ cardano-api/src/Cardano/Api.hs
@@ -1067,6 +1067,10 @@ module Cardano.Api
   , ResolvablePointers (..)
   , unsafeBoundedRational
 
+    -- ** Debug
+  , DebugPlutusFailure (..)
+  , renderDebugPlutusFailure
+
     -- ** Supporting modules
   , module Cardano.Api.Monad.Error
   , module Cardano.Api.Pretty
@@ -1121,6 +1125,7 @@ import           Cardano.Api.Monad.Error
 import           Cardano.Api.NetworkId
 import           Cardano.Api.OperationalCertificate
 import           Cardano.Api.Orphans ()
+import           Cardano.Api.Plutus
 import           Cardano.Api.Pretty
 import           Cardano.Api.Protocol
 import           Cardano.Api.ProtocolParameters
--- cardano-api/test/cardano-api-golden/Test/Golden/ErrorsSpec.hs
+++ cardano-api/test/cardano-api-golden/Test/Golden/ErrorsSpec.hs
@@ -31,7 +31,6 @@ module Test.Golden.ErrorsSpec
 where
 
 import           Cardano.Api
-import           Cardano.Api.Plutus
 import           Cardano.Api.Shelley
 
 import           Cardano.Binary as CBOR
