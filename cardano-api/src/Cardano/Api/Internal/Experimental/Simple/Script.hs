8dae23b5ca5feefe22be0669c6ac119d50d6aba1
Jordan Millar <jordan.millar@iohk.io>
2025-02-28 15:23:32 -0400

    Introduce the `SimpleScript era` type. This wraps cardano-ledger's associated type synonym NativeScript era.

---
+++ cardano-api/src/Cardano/Api/Internal/Experimental/Simple/Script.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE GADTs #-}
+
+module Cardano.Api.Internal.Experimental.Simple.Script
+  ( SimpleScript (..)
+  , SimpleScriptOrReferenceInput (..)
+  )
+where
+
+import Cardano.Api.Internal.TxIn (TxIn)
+
+import Cardano.Ledger.Core qualified as Ledger
+
+-- | A simple script in a particular era. We leverage ledger's Cardano.Api.Experimental.ErasraScript
+-- type class methods to work with the script.
+data SimpleScript era where
+  SimpleScript :: Ledger.NativeScript era -> SimpleScript era
+
+data SimpleScriptOrReferenceInput era
+  = SScript (SimpleScript era)
+  | SReferenceScript TxIn
