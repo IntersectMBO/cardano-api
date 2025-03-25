68245bc85c403bb8040168b093124d5f8dd66bee
Jordan Millar <jordan.millar@iohk.io>
2025-03-25 18:36:38 -0400

    WIP

--- scripts/enable_wasm_toolchain_ledger_only.sh
+++ scripts/enable_wasm_toolchain_ledger_only.sh
@@ -3,12 +3,24 @@
 
 source $HOME/.ghc-wasm/env
 
+export CONF_CC_OPTS_STAGE2="$CONF_CC_OPTS_STAGE2 -D_WASI_EMULATED_MMAN"
+export CONF_GCC_LINKER_OPTS_STAGE2="$CONF_GCC_LINKER_OPTS_STAGE2 -lwasi-emulated-mman"
+
 # This should be able to build!
 # cabal build --project-file=cabal-wasm.project cardano-api-wasm --keep-going
 
-cabal install happy
+# Check if `happy` is already installed
+if ! command -v happy &> /dev/null; then
+    echo "Happy is not installed. Installing it now..."
+    cabal install happy
+else
+    echo "Happy is already installed. Skipping installation."
+fi
 
-wasm32-wasi-cabal build --project-file=cabal-wasm-ledger-only.project cardano-api-wasm-ledger-only --keep-going
+
+wasm32-wasi-cabal build  --project-file=cabal-wasm-ledger-only.project \
+  cardano-api-wasm-ledger-only 
+
 
 # Hints
 # https://github.com/bradrn/brassica/blob/master/BUILDING.md
