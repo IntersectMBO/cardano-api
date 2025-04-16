348760671363288be7de4fa192c61542aab58618
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2025-04-16 19:00:22 +0200

    Improve autobalancing errors when change has no lovelace

--- cardano-api/test/cardano-api-test/Test/Cardano/Api/Orphans.hs
+++ cardano-api/test/cardano-api-test/Test/Cardano/Api/Orphans.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
 module Test.Cardano.Api.Orphans () where
 
 import Cardano.Api.Shelley
+
+import Cardano.Ledger.Mary.Value qualified as L
+
+import Data.String (IsString (..))
 
 import Test.Cardano.Crypto.Orphans ()
 
@@ -29,3 +34,5 @@ deriving instance Eq (SigningKey GenesisUTxOKey)
 deriving instance Eq (SigningKey KesKey)
 
 deriving instance Eq (SigningKey VrfKey)
+
+deriving instance IsString L.AssetName
