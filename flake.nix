4871ae7fa90fbd9881fde4102fb0a042e65a6faa
Alexey Kuleshevich <alexey.kuleshevich@iohk.io>
2025-04-17 12:58:33 +0530

    SRP Ledger,COnsensus and Network. Adjsut for missing byton test and add LOCALE to nix shell

--- flake.nix
+++ flake.nix
@@ -116,6 +116,19 @@
           shell.crossPlatforms = _: [];
           shell.shellHook = ''
             export PATH="$(git rev-parse --show-toplevel)/scripts/devshell:$PATH"
+            export LANG=en_US.UTF-8
+            export LC_ALL=en_US.UTF-8
+            export LOCALE_ARCHIVE="${nixpkgs.glibcLocales}/lib/locale/locale-archive"
+            DEFAULT_PS1="\n\[\033[1;32m\][nix-shell:\w]\$\[\033[0m\] "
+            prompt() {
+              local EXIT="$?"
+              if [ $EXIT != 0 ]; then
+                PS1="$DEFAULT_PS1\[\033[1;31m\]($EXIT)\[\033[00m\] "
+              else
+                PS1="$DEFAULT_PS1"
+              fi
+            }
+            PROMPT_COMMAND=prompt
           '';
 
           # package customizations as needed. Where cabal.project is not
