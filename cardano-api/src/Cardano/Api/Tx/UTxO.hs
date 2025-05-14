9f2eec19e735bdec3183295f3fcc4ec500d5886c
Daniel Firth <daniel.firth@iohk.io>
2025-05-14 11:47:52 +0000

    Add many more API functions to `Cardano.Api.Tx.UTxO` and format documentation.

--- cardano-api/src/Cardano/Api/Tx/UTxO.hs
+++ cardano-api/src/Cardano/Api/Tx/UTxO.hs
@@ -1,19 +1,70 @@
 module Cardano.Api.Tx.UTxO
-  ( UTxO.UTxO (..)
-  , UTxO.empty
-  , UTxO.singleton
+  ( -- * UTxO type
+    UTxO.UTxO (..)
+
+    -- * Operators
+  , (UTxO.\\)
+
+    -- * Query
   , UTxO.lookup
   , UTxO.resolveTxIn
-  , UTxO.filter
-  , UTxO.filterWithKey
+
+    -- * Construction
+  , UTxO.empty
+  , UTxO.singleton
+
+    -- ** Insertion
+  , UTxO.insert
+
+    -- ** Delete/Update
+  , UTxO.delete
+  , UTxO.adjust
+
+    -- * Combine
+
+    -- ** Union
+  , UTxO.union
+  , UTxO.unions
+
+    -- ** Difference
+  , UTxO.difference
+
+    -- ** Intersection
+  , UTxO.intersection
+
+    -- * Traversal
+
+    -- ** Map
+  , UTxO.map
+  , UTxO.mapWithKey
+  , UTxO.mapKeys
+
+    -- ** Fold
+  , UTxO.foldMap
+
+    -- * Conversion
   , UTxO.inputSet
   , UTxO.txOutputs
-  , UTxO.difference
+
+    -- ** Lists
   , UTxO.fromList
   , UTxO.toList
+
+    -- ** Maps
   , UTxO.toMap
+  , UTxO.fromMap
+
+    -- ** Shelley
   , UTxO.fromShelleyUTxO
   , UTxO.toShelleyUTxO
+
+    -- * Filter
+  , UTxO.filter
+  , UTxO.filterWithKey
+
+    -- * Find
+  , UTxO.find
+  , UTxO.findWithKey
   )
 where
 
