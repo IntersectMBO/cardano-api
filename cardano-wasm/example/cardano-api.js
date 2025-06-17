b27189f74a0ea6c5bcac1a80f6bc27a11e8942ce
Pablo Lamela <pablo.lamela@iohk.io>
2025-06-17 11:42:22 +0200

    Generate wrappers dynamically in JS using Haskell provided info

--- cardano-wasm/example/cardano-api.js
+++ cardano-wasm/example/cardano-api.js
@@ -3,31 +3,53 @@ import ghc_wasm_jsffi from "./cardano-wasm.js";
 const __exports = {};
 const wasi = new WASI([], [], []);
 async function initialize() {
-  let {instance} = await WebAssembly.instantiateStreaming(fetch("./cardano-wasm.wasm"), {
+  let { instance } = await WebAssembly.instantiateStreaming(fetch("./cardano-wasm.wasm"), {
     ghc_wasm_jsffi: ghc_wasm_jsffi(__exports),
     wasi_snapshot_preview1: wasi.wasiImport,
   })
   Object.assign(__exports, instance.exports);
   wasi.initialize(instance);
-  var makeSignedTxObject = async function (asyncValue) {
-    let value = await asyncValue;
-    return { objectType: "SignedTx"
-           , txToCbor: async function () { return instance.exports.txToCbor(value); }
-           };
-  }
-  var makeUnsignedTxObject = async function (asyncValue) {
-    let value = await asyncValue;
-    return { objectType: "UnsignedTx"
-           , addTxInput: function (txId, txIx) { return makeUnsignedTxObject(instance.exports.addTxInput(value, txId, txIx)); }
-           , addSimpleTxOut: function (destAddr, amount) { return makeUnsignedTxObject(instance.exports.addSimpleTxOut(value, destAddr, amount)); }
-           , setFee: function (amount) { return makeUnsignedTxObject(instance.exports.setFee(value, amount)); }
-           , addSigningKey: function (signingKey) { return makeUnsignedTxObject(instance.exports.addSigningKey(value, signingKey)); }
-           , signTx: function () { return makeSignedTxObject(instance.exports.signTx(value)); }
-           };
-  };
-  return { objectType: "cardano-api"
-         , newConwayTx: async function () { return makeUnsignedTxObject(instance.exports.newConwayTx()); }
-         };
 
+  // Dynamically build the API
+  const apiInfo = await instance.exports.getAPIInfo();
+  var makers = {};
+  var cardanoAPI = { objectType: "cardano-api" };
+
+  // Create maker functions for each virtual object type
+  apiInfo.virtualObjects.forEach(vo => {
+    makers[vo.objectName] = async function (asyncHaskellValue) {
+      const haskellValue = await asyncHaskellValue;
+      var jsObject = { objectType: vo.objectName };
+
+      vo.methods.forEach(method => {
+        jsObject[method.name] = function (...args) {
+          const resultPromise = instance.exports[method.name](haskellValue, ...args);
+
+          if (method.return.type === "fluent") { // Same object, fluent interface
+            return makers[vo.objectName](resultPromise);
+          } else if (method.return.type === "newObject") { // Convert to a new object
+            return makers[method.return.objectType](resultPromise);
+          } else { // Other return type, just return the result
+            return resultPromise;
+          }
+        };
+      });
+      return jsObject;
+    };
+  });
+
+  // Populate the main API object with static methods
+  apiInfo.staticMethods.forEach(method => {
+    cardanoAPI[method.name] = async function (...args) {
+      const resultPromise = instance.exports[method.name](...args);
+
+      if (method.return.type === "newObject") { // Create a new object
+        return makers[method.return.objectType](resultPromise);
+      } else { // Other return type, just return the result
+        return resultPromise;
+      }
+    };
+  });
+  return cardanoAPI;
 }
 export default initialize;
