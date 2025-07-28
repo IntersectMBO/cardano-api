6219a2322fc5b8e19638922dd9a426e63f794a72
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2025-07-28 18:59:31 +0200

    cardano-rpc | Add getPParamsJson gRPC endpoint

---
+++ cardano-rpc/src/Cardano/Rpc/Server/Internal/Node.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Cardano.Rpc.Server.Internal.Node
+  ( getEraMethod
+  , getPParamsJsonMethod
+  )
+where
+
+import Cardano.Api
+import Cardano.Rpc.Proto.Api.Node qualified as Rpc
+import Cardano.Rpc.Server.Internal.Error
+import Cardano.Rpc.Server.Internal.Monad
+import Cardano.Rpc.Server.Internal.Orphans ()
+
+import RIO hiding (toList)
+
+import Data.Aeson qualified as A
+import Data.ByteString.Lazy qualified as BL
+import Data.Default
+import Data.ProtoLens (defMessage)
+import Network.GRPC.Spec
+
+import Proto.Google.Protobuf.Empty
+
+getEraMethod :: MonadRpc e m => Proto Empty -> m (Proto Rpc.CurrentEra)
+getEraMethod _ = pure . Proto $ defMessage & #era .~ Rpc.Conway
+
+getPParamsJsonMethod :: MonadRpc e m => Proto Empty -> m (Proto Rpc.PParamsJson)
+getPParamsJsonMethod _ = do
+  nodeConnInfo <- grab
+  AnyCardanoEra era <- liftIO . throwExceptT $ determineEra nodeConnInfo
+  eon <- forEraInEon @ConwayEraOnwards era (error "Minimum Conway era required") pure
+  let sbe = convert eon
+
+  let target = VolatileTip
+  pparams <-
+    liftIO . (throwEither =<<) $
+      executeLocalStateQueryExpr nodeConnInfo target $
+        throwEither =<< throwEither =<< queryProtocolParameters sbe
+
+  let pparamsJson = conwayEraOnwardsConstraints eon $ A.encode pparams
+
+  pure $
+    def
+      & #json .~ BL.toStrict pparamsJson
