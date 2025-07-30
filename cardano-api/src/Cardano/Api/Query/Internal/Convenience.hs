78c18d7b8fbc5c6cfc1c46d9a18fbdf97371a736
Jordan Millar <jordan.millar@iohk.io>
2025-07-30 09:29:25 -0400

    Propagate `ChainAccountState`

--- cardano-api/src/Cardano/Api/Query/Internal/Convenience.hs
+++ cardano-api/src/Cardano/Api/Query/Internal/Convenience.hs
@@ -36,11 +36,10 @@ import Cardano.Api.Query.Internal.Type.QueryInMode
 import Cardano.Api.Tx.Internal.Body
 import Cardano.Api.UTxO (UTxO (..))
 
-import Cardano.Ledger.CertState (DRepState (..))
 import Cardano.Ledger.Coin qualified as L
+import Cardano.Ledger.Conway.State (ChainAccountState (..), DRepState (..))
 import Cardano.Ledger.Credential qualified as L
 import Cardano.Ledger.Keys qualified as L
-import Cardano.Ledger.Shelley.LedgerState qualified as L
 import Ouroboros.Consensus.HardFork.Combinator.AcrossEras (EraMismatch (..))
 import Ouroboros.Network.Protocol.LocalStateQuery.Type (Target (..))
 
@@ -168,11 +167,11 @@ queryStateForBalancedTx era allTxIns certs = runExceptT $ do
     caseShelleyToBabbageOrConwayEraOnwards
       (const $ pure Nothing)
       ( \cOnwards -> do
-          L.AccountState{L.asTreasury} <-
+          chainAccountState <-
             lift (queryAccountState cOnwards)
               & onLeft (left . QceUnsupportedNtcVersion)
               & onLeft (left . QueryEraMismatch)
-          let txCurrentTreasuryValue = TxCurrentTreasuryValue asTreasury
+          let txCurrentTreasuryValue = TxCurrentTreasuryValue $ casTreasury chainAccountState
           return $ Just $ Featured cOnwards txCurrentTreasuryValue
       )
       sbe
