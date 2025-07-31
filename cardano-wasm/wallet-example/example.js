ce5b2c5d48b310ebb11b57bd83ab77c166414014
Pablo Lamela <pablo.lamela@iohk.io>
2025-07-31 21:05:49 +0200

    Partial implementation

--- cardano-wasm/wallet-example/example.js
+++ cardano-wasm/wallet-example/example.js
@@ -3,47 +3,89 @@ import cardano_api from "./cardano-api.js";
 
 let promise = cardano_api();
 
-const output = document.createElement("code");
-output.innerText = "";
-output.id = "test-output";
-document.body.appendChild(output);
-
-function log(out) {
-  console.log(out);
-  if (typeof (out) == "object") {
-    output.innerText += "> [object] {\n";
-    for (let [key, val] of Object.entries(out)) {
-      let text = val.toString();
-      if (typeof (val) == "function") {
-        text = text.split("{")[0];
-      }
-      output.innerText += "    " + key + ": " + text + "\n";
-    }
-    output.innerText += "  }\n";
-  } else {
-    output.innerText += "> " + JSON.stringify(out) + "\n";
-  }
-}
-
-function finish_test() {
-  let finishTag = document.createElement("p");
-  finishTag.innerText = "Finished test!";
-  finishTag.id = "finish-tag";
-  document.body.appendChild(finishTag);
-}
-
 async function do_async_work() {
   let api = await promise;
-  log("Api object:");
-  log(api);
-
   let grpcApi = await api.newGrpcConnection("http://localhost:8080")
 
-  log("GRPC connection object:");
-  log(grpcApi);
+  let showPrivateKey = true;
+  let wallet = await api.generateTestnetPaymentWallet(2);
+  let refresh = async function () { };
+
+  async function generateAddress() {
+    wallet = await api.generateTestnetPaymentWallet(2);
+    await refresh();
+  }
+
+  // @ts-ignore
+  document.getElementById("regenerate-address-button").addEventListener("click", generateAddress);
+
+  async function togglePrivateKeyDisplay() {
+    showPrivateKey = !showPrivateKey;
+    await refresh();
+  }
+
+  // @ts-ignore
+  document.getElementById("show-private-key-button").addEventListener("click", togglePrivateKeyDisplay);
+
+  // @ts-ignore
+  async function loadPrivateKey() {
+    let pki = document.getElementById("private-key-input");
+    // @ts-ignore
+    wallet = await api.restoreTestnetPaymentWalletFromSigningKeyBech32(2, pki.value);
+    await refresh();
+  };
+
+  refresh = async function () {
+    // @ts-ignore
+
+    // Wallet rendering
+    document.getElementById("address-input").value = await wallet.getAddressBech32();
+    let pki = document.getElementById("private-key-input")
+    if (showPrivateKey) {
+      // @ts-ignore
+      pki.value = await wallet.getBech32ForSigningKey();
+      // @ts-ignore
+      pki.disabled = false;
+      // @ts-ignore
+      document.getElementById("show-private-key-button").innerText = "Hide";
+    } else {
+      // @ts-ignore
+      pki.value = "<<< Private key hidden >>>";
+      // @ts-ignore
+      pki.disabled = true;
+      // @ts-ignore
+      document.getElementById("show-private-key-button").innerText = "Show";
+    }
+    let utxos = await grpcApi.getUtxosForAddress(await wallet.getAddressBech32());
+    let utxoTable = document.getElementById("utxo-table");
+
+    // UTxO rendering
+    // @ts-ignore
+    while (utxoTable.rows.length > 1) {
+      // @ts-ignore
+      utxoTable.deleteRow(1);
+    }
+    // Add new rows for each UTxO
+    for (let utxo of utxos) {
+      let row = document.createElement("tr");
+      let txHashCell = document.createElement("td");
+      txHashCell.innerText = utxo.txHash;
+      row.appendChild(txHashCell);
+      let txIndexCell = document.createElement("td");
+      txIndexCell.innerText = utxo.txIndex.toString();
+      row.appendChild(txIndexCell);
+      let valueCell = document.createElement("td");
+      valueCell.innerText = utxo.value.toString();
+      row.appendChild(valueCell);
+      utxoTable.appendChild(row);
+    }
 
 
-  finish_test();
-}
+    // @ts-ignore
+    document.getElementById("load-private-key-button").addEventListener("click", loadPrivateKey);
 
-do_async_work().then(() => { });
+
+    generateAddress()
+  }
+
+  do_async_work().then(() => { });
