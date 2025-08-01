47bb7a6a5d176f3cb79a69835ff9b3c74df12aa3
Pablo Lamela <pablo.lamela@iohk.io>
2025-08-01 16:27:45 +0200

    Add total and fees to transaction outputs

--- cardano-wasm/wallet-example/example.js
+++ cardano-wasm/wallet-example/example.js
@@ -6,17 +6,27 @@ let promise = cardano_api();
 async function do_async_work() {
   let api = await promise;
   let grpcApi = await api.newGrpcConnection("http://localhost:8080");
+  let protocolParams = await grpcApi.getProtocolParams();
 
   // State
   let showPrivateKey = true;
   let wallet = await api.generateTestnetPaymentWallet(2);
   let transactionInputs = [];
   let transactionOutputs = [];
-  // always increasing index of a transaction to identify on a UI
-  let transactionOutputIx = 0;
+
+  async function makeTransaction() {
+    let tx = await api.newConwayTx();
+    for (let input of transactionInputs) {
+      tx = tx.addTxInput(input.txId, input.txIndex);
+    }
+    for (let output of transactionOutputs) {
+      tx = tx.addSimpleTxOut(output.address, output.lovelace);
+    }
+    return tx;
+  }
 
   // Refresh function
-  const refresh = async function () {
+  async function refresh() {
     // @ts-ignore
 
     // Wallet rendering
@@ -131,6 +141,84 @@ async function do_async_work() {
     totalRow.appendChild(labelCell);
     // @ts-ignore
     txinTable.appendChild(totalRow);
+
+    // Transaction Outputs
+    const txOutTable = document.getElementById('txout-table');
+    // @ts-ignore
+    while (txOutTable.rows.length > 1) {
+      // @ts-ignore
+      txOutTable.deleteRow(1);
+    }
+    let transactionOutputIx = 0;
+    let outputsTotal = 0n;
+    for (let output of transactionOutputs) {
+      // @ts-ignore
+      const newRow = txOutTable.insertRow();
+
+      // Address cell
+      const cellAddress = newRow.insertCell();
+      cellAddress.className = 'txout-txid long';
+      cellAddress.textContent = output.address;
+
+      // Lovelaces cell
+      const cellLovelaces = newRow.insertCell();
+      cellLovelaces.className = 'txout-txada';
+      cellLovelaces.textContent = output.lovelace;
+
+      // Remove the row of the clicked button
+      async function removeRow(index) {
+        transactionOutputs.splice(index, 1);
+        await refresh();
+      }
+
+      // Remove button cell
+      const cellRemove = newRow.insertCell();
+      cellRemove.className = 'txout-remove';
+      const btn = document.createElement('button');
+      btn.textContent = 'Remove';
+      let ix = transactionOutputIx;
+      btn.onclick = removeRow.bind(null, ix);
+      cellRemove.appendChild(btn);
+      outputsTotal += output.lovelace;
+      transactionOutputIx += 1;
+    }
+    let tx = await makeTransaction();
+    let fees = await tx.estimateMinFee(protocolParams, 1, 0, 0);
+    // @ts-ignore
+    const feesRow = txOutTable.insertRow();
+
+    // Empty cell
+    const cellEmpty = feesRow.insertCell();
+    cellEmpty.className = 'txout-txid long';
+    cellEmpty.textContent = '';
+
+    // Lovelaces cell
+    const cellLovelaces = feesRow.insertCell();
+    cellLovelaces.textContent = fees.toString();
+
+    // Label cell
+    const cellAddress = feesRow.insertCell();
+    cellAddress.className = 'total-label';
+    cellAddress.textContent = "Fees";
+
+    let totalSpent = outputsTotal + fees;
+    // @ts-ignore
+    const totalOutRow = txOutTable.insertRow();
+    // Empty cell
+    const cellEmpty2 = totalOutRow.insertCell();
+    cellEmpty2.className = 'txout-txid long';
+    cellEmpty2.textContent = '';
+
+    // Total spent cell
+    const cellTotalSpent = totalOutRow.insertCell();
+    cellTotalSpent.className = 'total';
+    cellTotalSpent.textContent = totalSpent.toString();
+
+    // Label cell for total spent
+    const cellTotalLabel = totalOutRow.insertCell();
+    cellTotalLabel.className = 'total-label';
+    cellTotalLabel.textContent = "Total";
+
   }
 
   // Callbacks
@@ -167,72 +255,33 @@ async function do_async_work() {
     await refresh();
   }
 
-  async function addRow(address, lovelace) {
-    const table = document.getElementById('txout-table');
-    // @ts-ignore
-    const rowInsertIx = table.rows[table.rows.length - 2].rowIndex;
-
-    // @ts-ignore
-    const newRow = table.insertRow(rowInsertIx);
-
-    // Address cell
-    const cellAddress = newRow.insertCell(0);
-    cellAddress.className = 'txout-txid long';
-    cellAddress.textContent = address;
-
-    // Lovelaces cell
-    const cellLovelaces = newRow.insertCell(1);
-    cellLovelaces.className = 'txout-txada';
-    cellLovelaces.textContent = lovelace;
-
-    // Remove the row of the clicked button
-    function removeRow(button, index) {
-      const row = button.closest('tr');
-      if (row) {
-        row.parentNode.removeChild(row);
-      }
-      transactionOutputs = transactionOutputs.filter(txOut => txOut.ix !== index);
-    }
-
-    // Remove button cell
-    const cellRemove = newRow.insertCell(2);
-    cellRemove.className = 'txout-remove';
-    const btn = document.createElement('button');
-    btn.textContent = 'Remove';
-    btn.onclick = removeRow.bind(null, btn, transactionOutputIx);
-    cellRemove.appendChild(btn);
-
+  async function addOutput(address, lovelace) {
     transactionOutputs.push({
-      ix: transactionOutputIx,
       address: address,
       lovelace: lovelace
     });
-    transactionOutputIx += 1;
   }
 
   document.getElementById('load-private-key-button')?.addEventListener('click', loadPrivateKey);
 
   document.getElementById('utxo-reload-button')?.addEventListener('click', refresh);
 
-  document.getElementById('add-output-button')?.addEventListener('click', () => {
-    addRow(
+  document.getElementById('add-output-button')?.addEventListener('click', async () => {
+    await addOutput(
       // @ts-ignore
       document.getElementById('add-output-address').value.trim(),
       // @ts-ignore
-      Number(document.getElementById('add-output-lovelace').value.trim()),
+      BigInt(document.getElementById('add-output-lovelace').value.trim()),
     );
 
     // @ts-ignore
     document.getElementById('add-output-address').value = '';
     // @ts-ignore
     document.getElementById('add-output-lovelace').value = '';
+
+    await refresh();
   });
 
   generateAddress();
-
-
-  // TODO REMOVE THIS
-  addRow("dc89a8979a786a76e7686c876", 234);
-  addRow("dc89a8979a786a76e7686c876", 468);
 }
 do_async_work().then(() => { });
