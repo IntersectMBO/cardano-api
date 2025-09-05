7fb2bf82a3e9db5adb0165f1b19cdf956019ee99
Pablo Lamela <pablo.lamela@iohk.io>
2025-09-05 20:39:58 +0200

    Output typescript declaration to a file instead of standard output

--- cardano-wasm/test/cardano-wasm-golden/Test/Golden/Cardano/Wasm/TypeScript.hs
+++ cardano-wasm/test/cardano-wasm-golden/Test/Golden/Cardano/Wasm/TypeScript.hs
@@ -1,10 +1,29 @@
 module Test.Golden.Cardano.Wasm.TypeScript where
 
+import Control.Monad (forM_)
+import Control.Monad.IO.Class qualified as H
+import Control.Monad.Trans.Control (control)
+import System.FilePath ((</>))
+import System.IO.Temp (withSystemTempDirectory)
+
 import Hedgehog as H
 import Hedgehog.Extras qualified as H
 
 hprop_cardano_wasm_typescript_declarations_match_generated :: Property
 hprop_cardano_wasm_typescript_declarations_match_generated =
   H.propertyOnce $ do
-    result <- H.execFlex "cardano-wasm" "CARDANO_WASM" []
-    H.diffVsGoldenFile result "lib-wrapper/cardano-api.d.ts"
+    control $ \runInHedgehog ->
+      H.liftIO $ withSystemTempDirectory "cardano-wasm-ts-decls" $ \dir -> do
+        runInHedgehog $ do
+          _ <- H.execFlex "cardano-wasm" "CARDANO_WASM" ["--output-dir", dir]
+          forFilesInDir dir $ \file -> do
+            H.annotate (dir </> file)
+            H.diffFileVsGoldenFile (dir </> file) ("lib-wrapper" </> file)
+
+forFilesInDir :: (H.MonadTest m, H.MonadIO m) => FilePath -> (FilePath -> m ()) -> m ()
+forFilesInDir dir action = do
+  files <- H.listDirectory dir
+  forM_ files $ \file -> do
+    let fullPath = dir </> file
+    H.assertFileExists fullPath
+    action file
