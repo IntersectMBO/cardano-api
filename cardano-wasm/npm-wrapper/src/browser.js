d4546f0f43c94e96e32a49109e5b26d74e5a6008
Pablo Lamela <pablo.lamela@iohk.io>
2025-10-08 22:58:29 +0200

    Fix web-grpc connecting for webpack

--- cardano-wasm/npm-wrapper/src/browser.js
+++ cardano-wasm/npm-wrapper/src/browser.js
@@ -1,6 +1,11 @@
 import { createInitializer } from './main.js';
 import { WASI } from '@bjorn3/browser_wasi_shim';
 import wasmUrl from './cardano-wasm.wasm';
+import grpcWebScript from './cardano_node_grpc_web_pb.js';
+
+const script = document.createElement('script');
+script.textContent = grpcWebScript;
+document.head.appendChild(script);
 
 const getWasiInstance = async () => {
     return new WASI([], [], []);
