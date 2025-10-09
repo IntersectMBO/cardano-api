8f6a7c4dbe17dd5c18a406483f4827b92c1cafe2
Pablo Lamela <pablo.lamela@iohk.io>
2025-10-09 17:06:42 +0200

    Split vanilla JS bit from rest of `cardano-api.js`

--- cardano-wasm/lib-wrapper/cardano-api.js
+++ cardano-wasm/lib-wrapper/cardano-api.js
@@ -1,116 +1,16 @@
-/// <reference path="./cardano-api.d.ts" />
+import { createInitializer } from './main.js';
 
-import { WASI } from "https://unpkg.com/@bjorn3/browser_wasi_shim@0.4.1/dist/index.js";
-import ghc_wasm_jsffi from "./cardano-wasm.js";
-const __exports = {};
-const wasi = new WASI([], [], []);
-async function initialise() {
-  let { instance } = await WebAssembly.instantiateStreaming(fetch("./cardano-wasm.wasm"), {
-    ghc_wasm_jsffi: ghc_wasm_jsffi(__exports),
-    wasi_snapshot_preview1: wasi.wasiImport,
-  })
-  Object.assign(__exports, instance.exports);
-  wasi.initialize(instance);
+const wasmUrl = './cardano-wasm.wasm';
 
-  // Wrap a function with variable arguments to make the parameters inspectable
-  function fixateArgs(params, func) {
-    const paramString = params.map(p => p.name).join(',');
-    // Dynamically create a function that captures 'func' from the closure.
-    // 'this' and 'arguments' are passed through from the wrapper to 'func'.
-    // Using eval allows the returned function to have named parameters for inspectability.
-    const wrapper = eval(`
-      (function(${paramString}) {
-        return func.apply(this, arguments);
-      })
-    `);
-    return wrapper;
-  }
+const getWasi = async () => {
+    return (await import("https://unpkg.com/@bjorn3/browser_wasi_shim@0.4.1/dist/index.js")).WASI;
+};
 
-  // Same as fixateArgs but for async functions
-  async function fixateArgsAsync(params, func) {
-    const paramString = params.map(p => p.name).join(',');
-    // Dynamically create an async function.
-    const wrapper = eval(`
-      (async function(${paramString}) {
-        return await func.apply(this, arguments);
-      })
-    `);
-    return wrapper;
-  }
+const loadWasmModule = async (importObject) => {
+  const response = await fetch("./cardano-wasm.wasm");
+  return await WebAssembly.instantiateStreaming(response, importObject);
+};
 
-  // Dynamically build the API
-  const apiInfo = await instance.exports.getApiInfo();
-  let makers = {};
-  let cardanoApi = { objectType: "cardano-api" };
-  // Create maker functions for each virtual object type
-  apiInfo.virtualObjects.forEach(vo => {
-    makers[vo.objectName] = function(initialHaskellValuePromise) {
-      // currentHaskellValueProvider is a function that returns a Promise for the Haskell value
-      // It starts with the initial value promise and fluent methods accumulate transformations
-      let currentHaskellValueProvider = () => initialHaskellValuePromise;
-      let jsObject = { objectType: vo.objectName };
-
-      vo.methods.forEach(method => {
-        if (method.return.type === "fluent") {
-          // Fluent methods are synchronous and update the provider
-          // A fluent method is one that returns the same object type
-          jsObject[method.name] = fixateArgs(method.params, function(...args) {
-            const previousProvider = currentHaskellValueProvider;
-            // We update the provider so that it resolves the previous provider and chains the next call
-            currentHaskellValueProvider = async () => {
-              const prevHaskellValue = await previousProvider();
-              return instance.exports[method.name](prevHaskellValue, ...args);
-            };
-            return jsObject; // Return current object for supporting chaining
-          });
-        } else {
-          // Non-fluent methods (newObject or other) are async and apply the accumulated method calls
-          jsObject[method.name] = fixateArgs(method.params, async function(...args) {
-            const haskellValue = await currentHaskellValueProvider(); // Resolve accumulated method calls
-            const resultPromise = instance.exports[method.name](haskellValue, ...args); // Call the non-fluent method
-
-            if (method.return.type === "newObject") { // It returns a new object
-              return makers[method.return.objectType](resultPromise);
-            } else { // It returns some primitive or other JS type (not a virtual object)
-              return resultPromise;
-            }
-          });
-        }
-      });
-      return jsObject;
-    };
-  });
-
-  // Populate the main API object with static methods
-  apiInfo.mainObject.methods.forEach(method => {
-    cardanoApi[method.name] = async function(...args) {
-      const resultPromise = instance.exports[method.name](...args);
-
-      if (method.return.type === "newObject") { // Create a new object
-        return makers[method.return.objectType](resultPromise);
-      } else { // Return some primitive or other JS type (not a virtual object)
-        return resultPromise;
-      }
-    };
-  });
-
-  return cardanoApi;
-}
-
-/**
- * Global utilities module used in JS foreign imports in WASM
- */
-globalThis.cardanoWasm = {
-  /**
-   * Convert Base64 to Base16 encoding
-   */
-  base64ToHex: function(base64) {
-    const binary = atob(base64);
-    return [...binary].reduce((hex, char) => {
-      const byteHex = char.charCodeAt(0).toString(16).padStart(2, '0');
-      return hex + byteHex;
-    }, '');
-  }
-}
-
+const initialise = createInitializer(getWasi, loadWasmModule);
 export default initialise;
+
