e28dd6f9219957b5a1403a160cab3830dcc6a888
Mateusz Galazyn <mateusz.galazyn@iohk.io>
2025-10-22 19:08:44 +0200

    Fix pattern match on consensus era

--- cardano-api/src/Cardano/Api/Consensus/Internal/Mode.hs
+++ cardano-api/src/Cardano/Api/Consensus/Internal/Mode.hs
@@ -167,5 +167,5 @@ fromConsensusEraIndex = \case
     AnyCardanoEra BabbageEra
   Consensus.EraIndex (S (S (S (S (S (S (Z (K ())))))))) ->
     AnyCardanoEra ConwayEra
-  Consensus.EraIndex (S (S (S (S (S (S (S _))))))) ->
+  Consensus.EraIndex (S (S (S (S (S (S (S (Z (K ()))))))))) ->
     AnyCardanoEra DijkstraEra
