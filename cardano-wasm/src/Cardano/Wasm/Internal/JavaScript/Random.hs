93a39282137b0a56dc3ca7c15dcf9cdd1bba8e67
Pablo Lamela <pablo.lamela@iohk.io>
2025-10-23 02:03:58 +0200

    Replace JS snippet with wasi call for random byte generation

--- cardano-wasm/src/Cardano/Wasm/Internal/JavaScript/Random.hs
+++ cardano-wasm/src/Cardano/Wasm/Internal/JavaScript/Random.hs
@@ -1,33 +1,58 @@
+{-# LANGUAGE CApiFFI #-}
 {-# LANGUAGE CPP #-}
 
 module Cardano.Wasm.Internal.JavaScript.Random (getRandomBytes) where
 
-import Data.ByteString (ByteString)
-
 #if !defined(wasm32_HOST_ARCH)
+
+import Data.ByteString (ByteString)
 
 getRandomBytes :: Word -> IO ByteString
 getRandomBytes _ = error "getRandomBytes is not implemented for non-WASM targets"
 
 #else
 
-import Cardano.Wasm.Internal.ExceptionHandling (rightOrError)
+import Control.Exception (throwIO)
+import Data.ByteString (ByteString, packCStringLen)
+import Data.Word (Word8)
+import System.IO.Error (mkIOError, userErrorType)
 
-import Data.ByteString.Base16 qualified as Base16
-import Data.ByteString.Char8 qualified as BSC
-import GHC.Wasm.Prim
+import Foreign.C.Types (CInt (..), CSize (..))
+import Foreign.Marshal.Alloc (allocaBytes)
+import Foreign.Ptr (Ptr, castPtr)
 
--- | Create a GRPC-web client for the Cardano API.
-foreign import javascript safe "{ let numBytes = $1; \
-                                  let randomBytes = new Uint8ClampedArray(numBytes); \
-                                  crypto.getRandomValues(randomBytes); \
-                                  return (randomBytes.reduce((acc, next) => acc.concat(next.toString(16).padStart(2, '0')), '')); \
-                                }"
-  js_generateRandomBytes :: Int -> IO JSString
+-- | Import the 'random_get' function from wasi
+-- module provided by the WASI runtime.
+--
+-- It takes a raw pointer to a u8 buffer (Ptr Word8) and the buffer's
+-- length (CSize) and returns a CInt representing the error code number.
+-- A return value of 0 means success.
+foreign import ccall "__wasi_random_get"
+  wasi_random_get
+    :: Ptr Word8
+    -- ^ Pointer to the buffer to fill
+    -> CSize
+    -- ^ Number of bytes to write
+    -> IO CInt
+    -- ^ Returns 0 on success, or an errno code
 
+-- | A safe Haskell wrapper around 'random_get' function from wasi.
+-- It requests the given number of cryptographically secure random
+-- bytes and returns them as a ByteString.
 getRandomBytes :: Word -> IO ByteString
-getRandomBytes n = do
-  hexRandomBytes <- fromJSString <$> js_generateRandomBytes (fromIntegral n)
-  rightOrError $ Base16.decode $ BSC.pack hexRandomBytes
+getRandomBytes n =
+  allocaBytes (fromIntegral n) $ \ptr -> do
+    -- Get pointer to allocated buffer
+    errno <- wasi_random_get ptr (fromIntegral n)
+    if errno == 0
+      -- If successful, pack the bytes from the pointer into a Haskell managed ByteString
+      then packCStringLen (castPtr ptr, fromIntegral n)
+      else -- Otherwise, we throw
+        throwIO $
+          mkIOError
+            userErrorType
+            ("wasi_random_get failed with errno: " ++ show errno)
+            Nothing
+            Nothing
 
 #endif
