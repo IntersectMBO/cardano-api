74641f440bec543dd5e6a60add336b5e3c037131
Pablo Lamela <pablo.lamela@iohk.io>
2025-11-27 04:05:36 +0100

    Reimplement `Cardano.Wasm.Api.GRPC` in terms of generated grpc API prototype

--- cardano-wasm/src-lib/Cardano/Wasm/Api/GRPC.hs
+++ cardano-wasm/src-lib/Cardano/Wasm/Api/GRPC.hs
@@ -1,55 +1,165 @@
 module Cardano.Wasm.Api.GRPC where
 
 import Cardano.Wasm.Api.Tx qualified as Wasm
-import Cardano.Wasm.ExceptionHandling (rightOrError, toMonadFail)
+import Cardano.Wasm.ExceptionHandling (justOrError, rightOrError, throwError)
+import Cardano.Wasm.Gen.Cardano.Rpc.CurrentEra (CurrentEra (..))
+import Cardano.Wasm.Gen.Cardano.Rpc.Node (getEra, getProtocolParamsJson)
+import Cardano.Wasm.Gen.Cardano.Rpc.ProtocolParamsJson (ProtocolParamsJson (..))
+import Cardano.Wasm.Gen.Google.Protobuf.Empty (Empty (Empty))
+import Cardano.Wasm.Gen.GrpcClient (GrpcExecutor)
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Cardano.AddressArray (AddressArray (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Cardano.TxOutput (TxOutput (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Query.AnyUtxoData (AnyUtxoData (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Query.QueryService (readUtxos)
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Query.ReadUtxosRequest (ReadUtxosRequest (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Query.ReadUtxosResponse (ReadUtxosResponse (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Query.TxoRef (TxoRef (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Submit.AnyChainTx (AnyChainTx (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Submit.SubmitService (submitTx)
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Submit.SubmitTxRequest (SubmitTxRequest (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Submit.SubmitTxResponse (SubmitTxResponse (..))
+import Cardano.Wasm.Gen.Utxorpc.V1alpha.Submit.TxSubmitResult (TxSubmitResult (..))
 
 import Data.ByteString.Base16 qualified as Base16
 import Data.ByteString.Base64 qualified as Base64
 import Data.ByteString.Char8 qualified as BS
-
--- | Internal data for the GrpcConnection virtual object. Currently, it is just a wrapper around the grpcClient,
--- which is some object that allows us to interact with the Cardano Node via gRPC or GRPC-web.
-newtype GrpcObject grpcClient
-  = GrpcObject grpcClient
+import Data.Text qualified as Text
 
 -- | Create a new unsigned transaction object for making a Conway era transaction.
-newGrpcConnectionImpl :: (String -> IO grpcClient) -> String -> IO (GrpcObject grpcClient)
-newGrpcConnectionImpl createClientFunc host = GrpcObject <$> createClientFunc host
+newGrpcConnectionImpl :: (String -> IO GrpcExecutor) -> String -> IO GrpcExecutor
+newGrpcConnectionImpl createClientFunc host = createClientFunc host
 
 -- | Get the era from the Cardano Node using gRPC or GRPC-web.
-getEraImpl :: (grpcClient -> IO Int) -> GrpcObject grpcClient -> IO Int
-getEraImpl getEraFunc (GrpcObject client) = getEraFunc client
+getEraImpl :: GrpcExecutor -> IO String
+getEraImpl caller = do
+  CurrentEra{currentEraEra = maybeEra} <- rightOrError =<< getEra caller Empty
+  era <- justOrError "getEra returned Nothing" maybeEra
+  return $ show era
 
 -- | Get the protocol parameters from the Cardano Node using gRPC or GRPC-web.
 getProtocolParamsImpl
-  :: (grpcClient -> IO Wasm.ProtocolParamsJSON)
-  -> GrpcObject grpcClient
+  :: GrpcExecutor
   -> IO Wasm.ProtocolParamsJSON
-getProtocolParamsImpl getProtocolParamsFunc (GrpcObject client) = getProtocolParamsFunc client
+getProtocolParamsImpl caller =
+  Wasm.ProtocolParamsJSON
+    . Text.unpack
+    . protocolParamsJsonJson
+    <$> ( rightOrError
+            =<< getProtocolParamsJson caller Empty
+        )
 
 -- | Get all UTXOs from the node using a gRPC or GRPC-web client.
-getAllUtxosImpl
-  :: (grpcClient -> IO utxos)
-  -> GrpcObject grpcClient
-  -> IO utxos
-getAllUtxosImpl getUtxosFunc (GrpcObject client) = getUtxosFunc client
+getAllUtxosImpl :: GrpcExecutor -> IO [(String, Int, String, String)]
+getAllUtxosImpl caller = do
+  ReadUtxosResponse
+    { readUtxosResponseItems = utxoDataList
+    } <-
+    rightOrError
+      =<< readUtxos
+        caller
+        ( ReadUtxosRequest
+            { readUtxosRequestTxoRefs = Nothing
+            , readUtxosRequestCardanoAddresses = Nothing
+            , readUtxosRequestFieldMask = Nothing
+            }
+        )
+  return
+    [ (Text.unpack refHash, refIndex, Text.unpack address, Text.unpack coin)
+    | Just
+        ( AnyUtxoData
+            { anyUtxoDataTxoRef =
+              Just
+                ( TxoRef
+                    { txoRefHash = refHash
+                    , txoRefIndex = refIndex
+                    }
+                  )
+            , anyUtxoDataCardano =
+              Just
+                ( TxOutput
+                    { txOutputAddress = address
+                    , txOutputCoin = coin
+                    }
+                  )
+            }
+          ) <-
+        utxoDataList
+    ]
 
 -- | Get UTXOs for a given address using a gRPC or GRPC-web client.
 getUtxosForAddressImpl
-  :: (grpcClient -> String -> IO utxos)
-  -> GrpcObject grpcClient
+  :: GrpcExecutor
   -> String
-  -> IO utxos
-getUtxosForAddressImpl getUtxosForAddressFunc (GrpcObject client) = getUtxosForAddressFunc client
+  -> IO [(String, Int, String)]
+getUtxosForAddressImpl caller address = do
+  ReadUtxosResponse
+    { readUtxosResponseItems = utxoDataList
+    } <-
+    rightOrError
+      =<< readUtxos
+        caller
+        ( ReadUtxosRequest
+            { readUtxosRequestTxoRefs = Nothing
+            , readUtxosRequestCardanoAddresses = Just (AddressArray [Text.pack address])
+            , readUtxosRequestFieldMask = Nothing
+            }
+        )
+  return
+    [ (Text.unpack refHash, refIndex, Text.unpack coin)
+    | Just
+        ( AnyUtxoData
+            { anyUtxoDataTxoRef =
+              Just
+                ( TxoRef
+                    { txoRefHash = refHash
+                    , txoRefIndex = refIndex
+                    }
+                  )
+            , anyUtxoDataCardano =
+              Just
+                ( TxOutput
+                    { txOutputCoin = coin
+                    }
+                  )
+            }
+          ) <-
+        utxoDataList
+    ]
 
 -- | Submit a transaction to the Cardano Node using gRPC or GRPC-web.
 submitTxImpl
-  :: (grpcClient -> String -> IO (Either String String))
-  -> GrpcObject grpcClient
+  :: GrpcExecutor
   -> String
   -> IO String
-submitTxImpl submitTxFunc (GrpcObject client) tx =
-  toMonadFail . rightOrError . (base64ToBase16 =<<) =<< submitTxFunc client tx
+submitTxImpl caller tx = do
+  submissionResult <-
+    submitTxResponseResults
+      <$> ( rightOrError
+              =<< submitTx
+                caller
+                ( SubmitTxRequest
+                    { submitTxRequestTx = [Just (AnyChainTx (Text.pack tx))]
+                    }
+                )
+          )
+  txRef <- case submissionResult of
+    [] -> throwError "submitTx returned no results"
+    [Nothing] -> throwError "submitTx returned no results"
+    [ Just
+        ( TxSubmitResult
+            { txSubmitResultRef = ref
+            , txSubmitResultErrorMessage = ""
+            }
+          )
+      ] -> return $ Text.unpack ref
+    [ Just
+        ( TxSubmitResult
+            { txSubmitResultErrorMessage = errMsg
+            }
+          )
+      ] -> throwError $ "submitTx error: " ++ Text.unpack errMsg
+    _ -> throwError "submitTx returned more than one result"
+  rightOrError $ base64ToBase16 txRef
  where
   -- We reencode as Base16 because it is a more common format for txIds
   base64ToBase16 :: String -> Either String String
