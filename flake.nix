32db16e1eaecb1578c67bd41cf23206ca9128c63
Pablo Lamela <pablo.lamela@iohk.io>
2025-12-03 01:52:56 +0100

    Reenable `proto-js-bundle` for `aarch64-darwin`

--- flake.nix
+++ flake.nix
@@ -291,24 +291,20 @@
                 };
             }
             // {
-              packages =
-                {wasm-typedoc = wasm-typedoc-drv;}
-                // lib.optionalAttrs (system != "aarch64-darwin") {
-                  proto-js-bundle = proto-js-bundle-drv;
-                };
+              packages = {
+                wasm-typedoc = wasm-typedoc-drv;
+                proto-js-bundle = proto-js-bundle-drv;
+              };
             };
           legacyPackages = {
             inherit cabalProject nixpkgs;
             # also provide hydraJobs through legacyPackages to allow building without system prefix:
             inherit hydraJobs;
           };
-          packages =
-            lib.optionalAttrs (system != "aarch64-darwin") {
-              proto-js-bundle = proto-js-bundle-drv;
-            }
-            // {
-              wasm-typedoc = wasm-typedoc-drv;
-            };
+          packages = {
+            proto-js-bundle = proto-js-bundle-drv;
+            wasm-typedoc = wasm-typedoc-drv;
+          };
           devShells = let
             # profiling shell
             profilingShell = p: {
