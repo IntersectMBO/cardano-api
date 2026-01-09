50a3b3cd44dd88888ee7078434ac5e91dd837784
Jordan Millar <jordan.millar@iohk.io>
2026-01-09 11:16:41 -0400

    Implement Eq instances for PlutusScriptWitness

--- cardano-api/src/Cardano/Api/Experimental/Plutus/Internal/ScriptWitness.hs
+++ cardano-api/src/Cardano/Api/Experimental/Plutus/Internal/ScriptWitness.hs
@@ -19,22 +19,24 @@ module Cardano.Api.Experimental.Plutus.Internal.ScriptWitness
   , ScriptRedeemer
   , PlutusScriptPurpose (..)
   , PlutusScriptDatum (..)
-  , NoScriptDatum (..)
-  , mkPlutusScriptWitness
+  , NoScriptDatum
 
     -- * Helpers
+  , getSpendingPlutusWitnessData
+  , getPlutusScriptRunnable
   , getPlutusScriptWitnessLanguage
   )
 where
 
-import Cardano.Api.Era.Internal.Eon.AlonzoEraOnwards
-import Cardano.Api.Era.Internal.Eon.ShelleyBasedEra
 import Cardano.Api.Experimental.Plutus.Internal.Script
 import Cardano.Api.Ledger qualified as L
 import Cardano.Api.Plutus.Internal.Script (ExecutionUnits)
 import Cardano.Api.Plutus.Internal.ScriptData
 
+import Cardano.Ledger.Plutus.Data qualified as Plutus
 import Cardano.Ledger.Plutus.Language qualified as L
+
+import GHC.IsList
 
 {-
 To construct a plutus script witness you need:
@@ -68,6 +70,42 @@ data PlutusScriptWitness (lang :: L.Language) (purpose :: PlutusScriptPurpose) e
 
 deriving instance Show (PlutusScriptWitness lang purpose era)
 
+instance Eq (PlutusScriptWitness L.PlutusV1 SpendingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness L.PlutusV2 SpendingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness L.PlutusV3 SpendingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness L.PlutusV4 SpendingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness lang MintingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness lang WithdrawingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness lang CertifyingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness lang ProposingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
+instance Eq (PlutusScriptWitness lang VotingScript era) where
+  (==) (PlutusScriptWitness s1 l1 d1 r1 eu1) (PlutusScriptWitness s2 l2 d2 r2 eu2) =
+    l1 == l2 && d1 == d2 && r1 == r2 && eu1 == eu2 && s1 == s2
+
 getPlutusScriptWitnessLanguage :: PlutusScriptWitness lang purpose era -> L.Language
 getPlutusScriptWitnessLanguage (PlutusScriptWitness l _ _ _ _) =
   case l of
@@ -75,6 +113,49 @@ getPlutusScriptWitnessLanguage (PlutusScriptWitness l _ _ _ _) =
     L.SPlutusV2 -> L.plutusLanguage l
     L.SPlutusV3 -> L.plutusLanguage l
     L.SPlutusV4 -> L.plutusLanguage l
+
+getSpendingPlutusWitnessData
+  :: forall era lang
+   . L.Era era
+  => PlutusScriptWitness lang SpendingScript era
+  -> L.TxDats era
+getSpendingPlutusWitnessData (PlutusScriptWitness L.SPlutusV1 _ d _ _) =
+  case d of
+    SpendingScriptDatum sd ->
+      let d' :: Plutus.Data era = toAlonzoData sd
+       in L.TxDats $ fromList [(Plutus.hashData d', d')]
+    InlineDatum -> mempty
+    NoScriptDatum -> mempty
+getSpendingPlutusWitnessData (PlutusScriptWitness L.SPlutusV2 _ d _ _) =
+  case d of
+    SpendingScriptDatum sd ->
+      let d' :: Plutus.Data era = toAlonzoData sd
+       in L.TxDats $ fromList [(Plutus.hashData d', d')]
+    InlineDatum -> mempty
+    NoScriptDatum -> mempty
+getSpendingPlutusWitnessData (PlutusScriptWitness L.SPlutusV3 _ d _ _) =
+  case d of
+    SpendingScriptDatum mSd -> case mSd of
+      Just sd ->
+        let d' :: Plutus.Data era = toAlonzoData sd
+         in L.TxDats $ fromList [(Plutus.hashData d', d')]
+      Nothing -> mempty
+    InlineDatum -> mempty
+    NoScriptDatum -> mempty
+getSpendingPlutusWitnessData (PlutusScriptWitness L.SPlutusV4 _ d _ _) =
+  case d of
+    SpendingScriptDatum mSd -> case mSd of
+      Just sd ->
+        let d' :: Plutus.Data era = toAlonzoData sd
+         in L.TxDats $ fromList [(Plutus.hashData d', d')]
+      Nothing -> mempty
+    InlineDatum -> mempty
+    NoScriptDatum -> mempty
+
+getPlutusScriptRunnable :: PlutusScriptWitness lang purpose era -> Maybe (L.PlutusRunnable lang)
+getPlutusScriptRunnable (PlutusScriptWitness _ (PScript (PlutusScriptInEra plutusScriptRunnable)) _ _ _) =
+  Just plutusScriptRunnable
+getPlutusScriptRunnable (PlutusScriptWitness _ PReferenceScript{} _ _ _) = Nothing
 
 -- | Every Plutus script has a purpose that indicates
 -- what that script is witnessing.
@@ -92,7 +173,7 @@ data PlutusScriptPurpose
   | -- | Witnesses a vote
     VotingScript
 
-data NoScriptDatum = NoScriptDatumAllowed deriving Show
+data NoScriptDatum
 
 -- | The PlutusScriptDatum type family is used to determine if a script datum is allowed
 -- for a given plutus script purpose and version. This change was proposed in CIP-69
@@ -130,21 +211,57 @@ data PlutusScriptDatum (lang :: L.Language) (purpose :: PlutusScriptPurpose) whe
   NoScriptDatum
     :: PlutusScriptDatum lang purpose
 
+instance Eq (PlutusScriptDatum L.PlutusV1 SpendingScript) where
+  (==) (SpendingScriptDatum d1) (SpendingScriptDatum d2) = d1 == d2
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum L.PlutusV2 SpendingScript) where
+  (==) (SpendingScriptDatum d1) (SpendingScriptDatum d2) = d1 == d2
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum L.PlutusV3 SpendingScript) where
+  (==) (SpendingScriptDatum d1) (SpendingScriptDatum d2) = d1 == d2
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum L.PlutusV4 SpendingScript) where
+  (==) (SpendingScriptDatum d1) (SpendingScriptDatum d2) = d1 == d2
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum lang MintingScript) where
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum lang WithdrawingScript) where
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum lang CertifyingScript) where
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum lang ProposingScript) where
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
+instance Eq (PlutusScriptDatum lang VotingScript) where
+  (==) InlineDatum InlineDatum = True
+  (==) NoScriptDatum NoScriptDatum = True
+  (==) _ _ = False
+
 instance Show (PlutusScriptDatum lang purpose) where
   show = \case
     SpendingScriptDatum _d -> "Datum"
     InlineDatum -> "InlineDatum"
     NoScriptDatum -> "NoScriptDatum"
-
-mkPlutusScriptWitness
-  :: AlonzoEraOnwards era
-  -> L.SLanguage plutuslang
-  -> L.PlutusRunnable plutuslang
-  -> PlutusScriptDatum plutuslang purpose
-  -> ScriptRedeemer
-  -> ExecutionUnits
-  -> PlutusScriptWitness plutuslang purpose (ShelleyLedgerEra era)
-mkPlutusScriptWitness _ l plutusScriptRunnable =
-  PlutusScriptWitness
-    l
-    (PScript $ PlutusScriptInEra plutusScriptRunnable)
