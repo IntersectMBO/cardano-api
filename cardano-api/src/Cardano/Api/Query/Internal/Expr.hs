0b6aa6bc8a860f35a99f45e88390e1bdd1f90b6e
Georgy Lukyanov <mail@geo2a.info>
2026-02-02 15:25:02 +0100

    [wip] Integrate GetLedgerPeerSnapshot query

--- cardano-api/src/Cardano/Api/Query/Internal/Expr.hs
+++ cardano-api/src/Cardano/Api/Query/Internal/Expr.hs
@@ -68,7 +68,7 @@ import Cardano.Slotting.Slot
 import Ouroboros.Consensus.Cardano.Block qualified as Consensus
 import Ouroboros.Consensus.HardFork.Combinator.AcrossEras as Consensus
 import Ouroboros.Network.Block (Serialised)
-import Ouroboros.Network.PeerSelection.LedgerPeers (LedgerPeerSnapshot)
+import Ouroboros.Network.PeerSelection.LedgerPeers (LedgerPeersKind, SomeLedgerPeerSnapshot)
 
 import Data.Map (Map)
 import Data.Sequence (Seq)
@@ -153,14 +153,15 @@ queryDebugLedgerState eon = querySbe eon QueryDebugLedgerState
 queryLedgerPeerSnapshot
   :: ()
   => ShelleyBasedEra era
+  -> LedgerPeersKind
   -> LocalStateQueryExpr
        block
        point
        QueryInMode
        r
        IO
-       (Either UnsupportedNtcVersionError (Either EraMismatch (Serialised LedgerPeerSnapshot)))
-queryLedgerPeerSnapshot eon = querySbe eon QueryLedgerPeerSnapshot
+       (Either UnsupportedNtcVersionError (Either EraMismatch (Serialised SomeLedgerPeerSnapshot)))
+queryLedgerPeerSnapshot eon = querySbe eon . QueryLedgerPeerSnapshot
 
 queryEraHistory
   :: ()
