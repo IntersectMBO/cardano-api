3be64ea0a2cd940f640272418606759b67b3c10a
Mateusz Galazyn <mateusz.galazyn@blockpqr.io>
2026-09-21 17:14:40 +0200

    Implement WaitForTx RPC method

--- cardano-rpc/src/Cardano/Rpc/Server.hs
+++ cardano-rpc/src/Cardano/Rpc/Server.hs
@@ -49,6 +49,7 @@ import Cardano.Rpc.Server.Internal.UtxoRpc.Mempool
 import Cardano.Rpc.Server.Internal.UtxoRpc.Query
 import Cardano.Rpc.Server.Internal.UtxoRpc.Submit
 import Cardano.Rpc.Server.Internal.UtxoRpc.Sync
+import Cardano.Rpc.Server.Internal.UtxoRpc.WaitForTx
 import Cardano.Rpc.Server.NodeKernelAccess
   ( NodeKernelAccess
   , mkNodeKernelAccess
@@ -100,7 +101,7 @@ methodsUtxoRpcSubmit =
   Method (mkNonStreaming $ wrapInSpan TraceRpcEvalTxSpan . evalTxMethod)
     . Method (mkNonStreaming $ wrapInSpan TraceRpcReadMempoolSpan . readMempoolMethod)
     . Method (mkNonStreaming $ wrapInSpan TraceRpcSubmitSpan . submitTxMethod)
-    . UnsupportedMethod -- waitForTx
+    . Method (mkServerStreaming $ \req -> wrapInSpan TraceRpcWaitForTxSpan . waitForTxMethod req)
     . Method (mkServerStreaming $ \req -> wrapInSpan TraceRpcWatchMempoolSpan . watchMempoolMethod req)
     $ NoMoreMethods
 
