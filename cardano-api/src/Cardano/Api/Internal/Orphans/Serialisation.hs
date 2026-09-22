f042ebd4ef4853c623d5141644cc865958afe7c5
Erik de Castro Lopo <erikd@mega-nerd.com>
2026-09-23 07:36:33 +1000

    Remove now-redundant orphan ToJSON Withdrawals instance
    cardano-ledger's Cardano.Ledger.Address now provides ToJSON Withdrawals
    natively (as of the bumped cardano-ledger SRP pin), conflicting with
    cardano-api's own orphan instance for it.

--- cardano-api/src/Cardano/Api/Internal/Orphans/Serialisation.hs
+++ cardano-api/src/Cardano/Api/Internal/Orphans/Serialisation.hs
@@ -201,8 +201,6 @@ deriving anyclass instance ToJSON L.Voting.Error
 
 deriving anyclass instance ToJSON L.VotingPeriod
 
-deriving anyclass instance ToJSON L.Withdrawals
-
 deriving anyclass instance
   ( ToJSON (L.PredicateFailure (L.EraRule "UTXOW" ledgerera))
   , ToJSON (L.PredicateFailure (L.EraRule "DELEGS" ledgerera))
